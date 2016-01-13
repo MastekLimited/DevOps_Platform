8ac84951e1f40ffe866562f99c97f7a1f4408b82
govindp <govind.parashar@mastek.com>
2016-01-13 11:20:56 +0000

    added TO DO task in setupAllVMs file
    Former-commit-id: 27ac7567d5f33b6b96c4cc7f1bf1907416380e42 [formerly 680c2e063559cbd6322417fb666238d3c83ab877] [formerly 78892e09c1f30870802b7903168b751e29ddc6a6 [formerly 78892e09c1f30870802b7903168b751e29ddc6a6 [formerly 78892e09c1f30870802b7903168b751e29ddc6a6 [formerly bb8a45a48fbd5185c5016f47e77f4c5d9e211ee9]]]]
    Former-commit-id: 78892e09c1f30870802b7903168b751e29ddc6a6
    Former-commit-id: b30ca07f998176d553e135f97248ecd53e0d232e
    Former-commit-id: e5d196abc08c296fd933ecc198f9117b251c3cfe
    Former-commit-id: dea14700e8146d32e359dce5224c3de9123f6c72

--- setupAllVMs.sh
+++ setupAllVMs.sh
@@ -1,4 +1,7 @@
 #/bin/bash
+#TO DO 
+#received input env. name
+#get file ips from env file
 startCleanVirtualBox() {	
 local VAGARANTFILE_PATH=$1
 cd $VAGARANTFILE_PATH;
@@ -6,6 +9,7 @@ echo "destroying the VM if already exists from location - "$VAGARANTFILE_PATH
 vagrant destroy -f;
 echo "Starting the VM at this location - "$VAGARANTFILE_PATH
 vagrant up;
+#JENKINS_IP=$1 vagrant up
 exitingFromThePassedFolder $VAGARANTFILE_PATH 
 }
 
@@ -26,3 +30,9 @@ startCleanVirtualBox vagrant/elk/ #192.168.51.102
 startCleanVirtualBox vagrant/postgres/ #192.168.51.106
 startCleanVirtualBox vagrant/omd/  #192.168.51.105
 startCleanVirtualBox vagrant/docker/ #192.168.29.110
+
+# Call startCleanVirtualBox vagrant/jenkins_sonar/ $JENKINS_IP
+
+#And use ENV['MY_VAR'] in recipe.
+#Add below line into vagrant file
+ #config.vm.network  "public_network", bridge: 'eth1', ip: ENV['JENKINS_IP'] 
