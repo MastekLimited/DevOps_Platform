425ecdaf83eecb3fde30a387883cd7bb6de41576
krishnak <krishna.kuntala@mastek.com>
2016-01-14 21:56:07 +0000

    Replaced private LAN IP addresses with public n/w IP addresses.
    Former-commit-id: b37f34ec104b898a23699992785831658432b329 [formerly 857638361cad3f050687e87846bdc173b58d811e] [formerly 49d3f4a2a628b5b57bd5113aa899165a9ce04d38 [formerly 49d3f4a2a628b5b57bd5113aa899165a9ce04d38 [formerly 49d3f4a2a628b5b57bd5113aa899165a9ce04d38 [formerly 49d3f4a2a628b5b57bd5113aa899165a9ce04d38 [formerly 49d3f4a2a628b5b57bd5113aa899165a9ce04d38 [formerly 49d3f4a2a628b5b57bd5113aa899165a9ce04d38 [formerly 49d3f4a2a628b5b57bd5113aa899165a9ce04d38 [formerly d52efd5e32250ba0221736e298280463cc9335fa]]]]]]]]
    Former-commit-id: 49d3f4a2a628b5b57bd5113aa899165a9ce04d38
    Former-commit-id: be30deec2084e6c784c65fc3bd6dd577e88ee1e7
    Former-commit-id: a53febe86860cf246d84d8ffcb6a002c286a50fa [formerly 583d2388b55c862ed85a96efa7f4740f051fb83d] [formerly b72abed578737b017f494c997a4121def8229873 [formerly 15ba5558c404d2fb3f733285d4f69180cddf3607]]
    Former-commit-id: 35382334f715fbae373b810fdf48cf53162325c2
    Former-commit-id: c4ea2652a6f060cf1d50c8262295d1ba64f0b492 [formerly 70d2cd6095038734b72b6f12f1b03c5e219f1abb] [formerly c4ea2652a6f060cf1d50c8262295d1ba64f0b492 [formerly 70d2cd6095038734b72b6f12f1b03c5e219f1abb] [formerly 076508c3611221a4ec9be9f8e773ccc33704b21a [formerly 7844cbc69e20b1be8c57784da0b4a4b0d093aae1]]]
    Former-commit-id: dfcb90383c59d267531c55af4871f5efb6dc0bf7
    Former-commit-id: fc24a669ad766cd0da066a84263fd83d2e782f0c
    Former-commit-id: 3243f34e8860e3e6418129345eab80c6f374da13

--- scripts/jenkins-setup/artifactTransfer.sh
+++ scripts/jenkins-setup/artifactTransfer.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo ">>>>>>>>>>>>>>>> Transfering Artifact >>>>>>>>>>>>>>>>>>>>"
 scp  -o StrictHostKeyChecking=no /var/lib/jenkins/jobs/DevOps_Development/workspace/Microservices/employee-service/target/employeeService.jar vagrant@192.168.29.110:/vshare/Build/employee/
-scp  -o StrictHostKeyChecking=no /var/lib/jenkins/jobs/DevOps_Development/workspace/Microservices/project-service/target/projectService.jar vagrant@192.168.29.110:/vshare/Build/project/
-scp  -o StrictHostKeyChecking=no /var/lib/jenkins/jobs/DevOps_Development/workspace/Microservices/project-assignment-service/target/projectAssignmentService.jar vagrant@192.168.29.110:/vshare/Build/project-assignment-service/
-scp  -o StrictHostKeyChecking=no /var/lib/jenkins/jobs/DevOps_Development/workspace/WebApplications/organisation-web-application/target/organisationWebApp.war vagrant@192.168.29.110:/vshare/Build/organisation-web/
+scp  -o StrictHostKeyChecking=no /var/lib/jenkins/jobs/DevOps_Development/workspace/Microservices/project-service/target/projectService.jar vagrant@172.16.72.247:/vshare/Build/project/
+scp  -o StrictHostKeyChecking=no /var/lib/jenkins/jobs/DevOps_Development/workspace/Microservices/project-assignment-service/target/projectAssignmentService.jar vagrant@172.16.72.247:/vshare/Build/project-assignment-service/
+scp  -o StrictHostKeyChecking=no /var/lib/jenkins/jobs/DevOps_Development/workspace/WebApplications/organisation-web-application/target/organisationWebApp.war vagrant@172.16.72.247:/vshare/Build/organisation-web/
 echo ">>>>>>>>>>>>>>>> Transfered Artifact >>>>>>>>>>>>>>>>>>>>"
 echo ".................. Re run the docker container ........................"
-ssh -o StrictHostKeyChecking=no vagrant@192.168.29.110 'cd /vshare/docker/ && ./runAllScripts.sh'
+ssh -o StrictHostKeyChecking=no vagrant@172.16.72.247 'cd /vshare/docker/ && ./runAllScripts.sh'
 echo ".................. Successfully Executed ........................"
 exit
--- scripts/omd_seup/hosts.mk
+++ scripts/omd_seup/hosts.mk
@@ -9,23 +9,23 @@ all_hosts += [
 ]
 
 # Explicit IP addresses
-ipaddresses.update({'EMP': u'192.168.29.110',
+ipaddresses.update({'EMP': u'172.16.72.247',
  'OMD_SERVER': u'127.0.0.1',
- 'PROJECT': u'192.168.29.110',
- 'WEB': u'192.168.29.110'})
+ 'PROJECT': u'172.16.72.247',
+ 'WEB': u'172.16.72.247'})
 
 
 # Host attributes (needed for WATO)
 host_attributes.update(
-{'EMP': {'ipaddress': u'192.168.29.110',
+{'EMP': {'ipaddress': u'172.16.72.247',
          'tag_agent': 'cmk-agent',
          'tag_networking': 'wan'},
  'OMD_SERVER': {'ipaddress': u'127.0.0.1',
                 'tag_agent': 'cmk-agent',
                 'tag_networking': 'wan'},
- 'PROJECT': {'ipaddress': u'192.168.29.110',
+ 'PROJECT': {'ipaddress': u'172.16.72.247',
              'tag_agent': 'cmk-agent',
              'tag_networking': 'wan'},
- 'WEB': {'ipaddress': u'192.168.29.110',
+ 'WEB': {'ipaddress': u'172.16.72.247',
          'tag_agent': 'cmk-agent',
          'tag_networking': 'wan'}})
