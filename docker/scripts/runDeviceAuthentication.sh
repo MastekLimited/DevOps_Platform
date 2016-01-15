e953207a28085a717e665eef9b7cd993d12a82f3
krishnak <krishna.kuntala@mastek.com>
2016-01-15 11:20:54 +0000

    Added docker container for device authentication
    Former-commit-id: 71bd82a5d135225f8d8a3129f8260a53549a70c8 [formerly a2614dad3585447085a4fad1535ae064475f82c8]
    Former-commit-id: 6ae3cbc95ccbc5ff9c4e0e07fad1eea857258a0c

---
+++ docker/scripts/runDeviceAuthentication.sh
@@ -0,0 +1,1 @@
+docker run -p 8095:8095 -p 9095:6556 --name deviceRegistration -h deviceRegistration -v /vshare/docker/config:/home/devops/config -v /vshare/Build/device-authentication-service/:/usr/share/ device-authentication 
