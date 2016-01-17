ea2b0905528ffac92c8dbcc52b009437a10809aa
unknown <Govind10995@GRB-RDG1WIN7G01.mastek.com>
2016-01-17 18:29:01 +0000

    IP's Replaced by variable
    Former-commit-id: c34968853f334eed505f4acdc88334b60e4cba1b [formerly b485a7b47c91f742922764898f75bac86e9170c9] [formerly 1d79c5a92c23f373097f32f61a337dc0c08b41bf] [formerly 8e6561af87e1d941a29e08944ed8153767cc38ba [formerly 8e6561af87e1d941a29e08944ed8153767cc38ba [formerly a2a53dce3210ebc7aa5c411c250813dc23643354]]] [formerly 0bf5fe4f6080aa95c2563581eddda78cb2f79993] [formerly 5b9122bbcda90c412a36fcec32973027c30fd5c6 [formerly cdb1f8333551b586000fce2d5aa711a636ad6fc8] [formerly 34e7989e71284020aa39bf489315a07768dd805d [formerly a66bdba133fec12b910db1e8d797b93eb67d7a33]]] [formerly bf9b043f8d435077f35ff8989143bdd3b24a7ea2] [formerly 0c6284d55db3708e375044023c85a91fddca8e4c] [formerly dc7c75f5e1a01dfa2d86f520f67495e5ab0d4028 [formerly 275b3f13797f2727e6c45dc3f9dc1a049ea3f7ea] [formerly 0c6284d55db3708e375044023c85a91fddca8e4c [formerly 0c6284d55db3708e375044023c85a91fddca8e4c [formerly 0c6284d55db3708e375044023c85a91fddca8e4c [formerly 0c6284d55db3708e375044023c85a91fddca8e4c [formerly 0c6284d55db3708e375044023c85a91fddca8e4c [formerly 0c6284d55db3708e375044023c85a91fddca8e4c [formerly 05dfbade62d1fb55e9633fc036fb9c6e160d7f12]]]]]]]]
    Former-commit-id: 35513af816d3b4e58967cfa1fbae7e912500bc34 [formerly 8dec47de43b9a40fc3a779a9a60e37d2c6a6d7a2] [formerly 551201788bdf21892250002bab8e76e8509044fd [formerly 551201788bdf21892250002bab8e76e8509044fd [formerly 551201788bdf21892250002bab8e76e8509044fd [formerly 551201788bdf21892250002bab8e76e8509044fd [formerly 551201788bdf21892250002bab8e76e8509044fd [formerly 551201788bdf21892250002bab8e76e8509044fd [formerly 1b44f4ab946a6d9a609684199a8b91c57ffbfc77]]]]]]]
    Former-commit-id: 551201788bdf21892250002bab8e76e8509044fd
    Former-commit-id: e1b36047733a2aa1d3543df28ffd9a45ea24dfa0
    Former-commit-id: 91c651e212b4a4bcba58ecda355071c2c51c63b1 [formerly 2d78b6c39d5e0959db4e9c879bc1fdd801a8b1e9] [formerly 5c8affc492983075e82429ff24bdcbe3724166e7 [formerly 457cb3e79eee5dc8fe04407cc8404f899fa3bad4]]
    Former-commit-id: 8a14c8f07c60577e910f3ff87a47b3898a3a489c
    Former-commit-id: 632b5c6f6c3883123f745280d49bd96b05e38bba [formerly 632b5c6f6c3883123f745280d49bd96b05e38bba [formerly 411647b7ea58911ba6fd2cdcf9242d04510de7f5]]
    Former-commit-id: d31169d33e39f31dc57993c4782ced1cc3c68e35
    Former-commit-id: 42b2b1923a4ca3dcbe17db80e777f82fc2bde796

--- scripts/jenkins-setup/artifactTransfer.sh
+++ scripts/jenkins-setup/artifactTransfer.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo ">>>>>>>>>>>>>>>> Transfering Artifact >>>>>>>>>>>>>>>>>>>>"
-scp  -o StrictHostKeyChecking=no /var/lib/jenkins/jobs/DevOps_Development/workspace/Microservices/employee-service/target/employeeService.jar vagrant@192.168.29.110:/vshare/Build/employee/
-scp  -o StrictHostKeyChecking=no /var/lib/jenkins/jobs/DevOps_Development/workspace/Microservices/project-service/target/projectService.jar vagrant@192.168.29.110:/vshare/Build/project/
-scp  -o StrictHostKeyChecking=no /var/lib/jenkins/jobs/DevOps_Development/workspace/Microservices/project-assignment-service/target/projectAssignmentService.jar vagrant@192.168.29.110:/vshare/Build/project-assignment-service/
-scp  -o StrictHostKeyChecking=no /var/lib/jenkins/jobs/DevOps_Development/workspace/WebApplications/organisation-web-application/target/organisationWebApp.war vagrant@192.168.29.110:/vshare/Build/organisation-web/
+scp  -o StrictHostKeyChecking=no /var/lib/jenkins/jobs/DevOps_Development/workspace/Microservices/employee-service/target/employeeService.jar vagrant@&&DOCKER_HOST_IP&&:/vshare/Build/employee/
+scp  -o StrictHostKeyChecking=no /var/lib/jenkins/jobs/DevOps_Development/workspace/Microservices/project-service/target/projectService.jar vagrant@&&DOCKER_HOST_IP&&:/vshare/Build/project/
+scp  -o StrictHostKeyChecking=no /var/lib/jenkins/jobs/DevOps_Development/workspace/Microservices/project-assignment-service/target/projectAssignmentService.jar vagrant@&&DOCKER_HOST_IP&&:/vshare/Build/project-assignment-service/
+scp  -o StrictHostKeyChecking=no /var/lib/jenkins/jobs/DevOps_Development/workspace/WebApplications/organisation-web-application/target/organisationWebApp.war vagrant@&&DOCKER_HOST_IP&&:/vshare/Build/organisation-web/
 echo ">>>>>>>>>>>>>>>> Transfered Artifact >>>>>>>>>>>>>>>>>>>>"
 echo ".................. Re run the docker container ........................"
-ssh -o StrictHostKeyChecking=no vagrant@192.168.29.110 'cd /vshare/docker/ && ./runAllScripts.sh'
+ssh -o StrictHostKeyChecking=no vagrant@&&DOCKER_HOST_IP&& 'cd /vshare/docker/ && ./runAllScripts.sh'
 echo ".................. Successfully Executed ........................"
 exit
