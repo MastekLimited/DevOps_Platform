0f4e8529d8dbf73e0012d916b9b742b2a77d740d
unknown <Govind10995@GRB-RDG1WIN7G01.mastek.com>
2016-01-17 18:16:04 +0000

    Conditionally Network configuration in vagrantfile
    Former-commit-id: 00618df886431a76a975816483b574de211e457f [formerly 374a117ada36abd236db82b4ed94cba4d8f7d0c3] [formerly 72336802e7b4a374a2e8c6586c40a74022b3cf34 [formerly 6d3435a626e8033ae837869df08074d1023939b8] [formerly 6d3435a626e8033ae837869df08074d1023939b8 [formerly 6d3435a626e8033ae837869df08074d1023939b8 [formerly 35482f7dfea144eb66d3831600b01b89b0a97483]]]]
    Former-commit-id: 72336802e7b4a374a2e8c6586c40a74022b3cf34 [formerly 6d3435a626e8033ae837869df08074d1023939b8]
    Former-commit-id: c46fe0c94c212b3fcc400f5c6d173e2dd0b8e022
    Former-commit-id: 85dd8d83e7c6f0f50adbaff94648fb2669f34331 [formerly 3f5762ec7cf3654ea344c8309370253f892a2362]
    Former-commit-id: 385dc4ff089fed46f0cdf7b8941e6071a015499b

--- setupAllVMs.sh
+++ setupAllVMs.sh
@@ -1,16 +1,23 @@
 #/bin/bash
-envname=env_inventory/$1/host_address.txt
-echo "Starting  Env.....$envname"
+if [ -z "$1" ]
+  then
+	echo "No argument supplied"
+    echo "Please pass the environment type i.e. local,dev etc."
+	exit
+fi
+envfilename=env_inventory/$1/host_address.txt
+envname=$1
+networkScriptFileName="enp4s0"
+echo "Start building  environment of .....$envname"
 startCleanVirtualBox() {	
 local VAGARANTFILE_PATH=$1
 local serverip=$2
-echo ">>>>>>>>>>>>>>>>>>$serverip"
 cd $VAGARANTFILE_PATH;
 echo "destroying the VM if already exists from location - "$VAGARANTFILE_PATH
 vagrant destroy -f;
 echo "Starting the VM at this location - "$VAGARANTFILE_PATH
 #vagrant up;
-SYS_IP=$2 vagrant up
+SYS_IP=$serverip  ENV_TYPE=$envname NET_CONFIG_FILE=$networkScriptFileName  vagrant up
 exitingFromThePassedFolder $VAGARANTFILE_PATH 
 }
 exitingFromThePassedFolder()
@@ -32,17 +39,17 @@ do
 	var=$(echo $name | awk -F"=" '{print $1,$2}')   
 	set -- $var
 	IPMAP[$1]=$2	 
-done < $envname
+done < $envfilename
 echo "JENKINS SERVER :-  ${IPMAP["JENKINS_HOST_IP"]}"
 echo "ELK SERVER :- ${IPMAP["ELK_HOST_IP"]}"
 echo "POSTGRES SERVER :- ${IPMAP["POSTGRES_HOST_IP"]}"
 echo "OMD SERVER :- ${IPMAP["OMD_HOST_IP"]}"
 echo "DOCKER SERVER :- ${IPMAP["DOCKER_HOST_IP"]}"
 
-#grep -r '&&JENKINS_HOST_IP&&' -l --null ./ | xargs -0 sed -i 's#${IPMAP["JENKINS_HOST_IP"]}#'${IPMAP["JENKINS_HOST_IP"]}'#g'
-#grep -r '&&ELK_HOST_IP&&' -l --null ./ | xargs -0 sed -i 's#${IPMAP["ELK_HOST_IP"]}#'${IPMAP["ELK_HOST_IP"]}'#g'
-#grep -r '&&POSTGRES_HOST_IP&&' -l --null . | xargs -0 sed -i 's#${IPMAP["POSTGRES_HOST_IP"]}#'${IPMAP["POSTGRES_HOST_IP"]}'#g'
-#grep -r '&&OMD_HOST_IP&&' -l --null ./ | xargs -0 sed -i 's#${IPMAP["OMD_HOST_IP"]}#'${IPMAP["OMD_HOST_IP"]}'#g'
+#grep -r '&&JENKINS_HOST_IP&&' -l --null ./ | xargs -0 sed -i 's#&&JENKINS_HOST_IP&&#'${IPMAP["JENKINS_HOST_IP"]}'#g'
+#grep -r '&&ELK_HOST_IP&&' -l --null ./ | xargs -0 sed -i 's#&&ELK_HOST_IP&&#'${IPMAP["ELK_HOST_IP"]}'#g'
+#grep -r '&&POSTGRES_HOST_IP&&' -l --null ./ | xargs -0 sed -i 's#&&POSTGRES_HOST_IP&&#'${IPMAP["POSTGRES_HOST_IP"]}'#g'
+#grep -r '&&OMD_HOST_IP&&' -l --null ./ | xargs -0 sed -i 's#&&OMD_HOST_IP&&#'${IPMAP["OMD_HOST_IP"]}'#g'
 #grep -r '&&DOCKER_HOST_IP&&' -l --null ./ | xargs -0 sed -i 's#&&DOCKER_HOST_IP&&#'${IPMAP["DOCKER_HOST_IP"]}'#g'
 
 #find . -name "*.sh" -exec chmod +x {} \;
@@ -52,3 +59,4 @@ startCleanVirtualBox vagrant/postgres/ ${IPMAP["POSTGRES_HOST_IP"]}
 startCleanVirtualBox vagrant/omd/  ${IPMAP["OMD_HOST_IP"]}
 startCleanVirtualBox vagrant/docker/ ${IPMAP["DOCKER_HOST_IP"]}
 #git reset --hard
+#git clean -f
