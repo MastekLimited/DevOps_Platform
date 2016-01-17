551201788bdf21892250002bab8e76e8509044fd
unknown <Govind10995@GRB-RDG1WIN7G01.mastek.com>
2016-01-17 18:29:01 +0000

    IP's Replaced by variable
    Former-commit-id: 0c6284d55db3708e375044023c85a91fddca8e4c [formerly 05dfbade62d1fb55e9633fc036fb9c6e160d7f12]
    Former-commit-id: 1b44f4ab946a6d9a609684199a8b91c57ffbfc77

--- scripts/elk_setup/centos/logstash-forwarder-ubuntu.sh
+++ scripts/elk_setup/centos/logstash-forwarder-ubuntu.sh
@@ -22,7 +22,7 @@ fi
 
 cp /mnt/gluster/repo/logstash-forwarder.crt /etc/pki/tls/certs/
 #OR
-#scp root@192.168.51.102:/mnt/gluster/repo/logstash-forwarder.crt /etc/pki/tls/certs/
+#scp root@&&ELK_HOST_IP&&:/mnt/gluster/repo/logstash-forwarder.crt /etc/pki/tls/certs/
 
 cp /mnt/gluster/repo/logstash-forwarder.conf /etc
 
