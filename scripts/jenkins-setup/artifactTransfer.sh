9be69a1c02e5760f34eecfe6edf64d795de30678
root <krishna.kuntala@mastek.com>
2016-01-18 12:10:21 +0000

    dos2unix
    Former-commit-id: f8e6ae343c818266cf88b5bd779e5ad05b46f6f5 [formerly befcfe4a55ebaffcf19eee250596173761a07d35] [formerly fc4456d93d4fecbef477bcca7723277d53675691 [formerly fc4456d93d4fecbef477bcca7723277d53675691 [formerly fc4456d93d4fecbef477bcca7723277d53675691 [formerly 3d28ea00f3ea9777963efbadf508a60a6e5f1571]]]]
    Former-commit-id: fc4456d93d4fecbef477bcca7723277d53675691
    Former-commit-id: 0d92173d64904f8c2c7f0b76c416dcc092c3fb41
    Former-commit-id: 504a03577b12a8a98a97a81bbad0008efef7eba5 [formerly ac12e3fe82eba5f44be1b5e97ad30a619c772ae5]
    Former-commit-id: 5e62c79b1c7cffd21ec57755d65f36de05efd6e2

--- scripts/jenkins-setup/artifactTransfer.sh
+++ scripts/jenkins-setup/artifactTransfer.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo ">>>>>>>>>>>>>>>> Transfering Artifact >>>>>>>>>>>>>>>>>>>>"
-scp  -o StrictHostKeyChecking=no /var/lib/jenkins/jobs/DevOps_Development/workspace/Microservices/employee-service/target/employeeService.jar vagrant@&&DOCKER_HOST_IP&&:/vshare/Build/employee/
-scp  -o StrictHostKeyChecking=no /var/lib/jenkins/jobs/DevOps_Development/workspace/Microservices/project-service/target/projectService.jar vagrant@&&DOCKER_HOST_IP&&:/vshare/Build/project/
-scp  -o StrictHostKeyChecking=no /var/lib/jenkins/jobs/DevOps_Development/workspace/Microservices/project-assignment-service/target/projectAssignmentService.jar vagrant@&&DOCKER_HOST_IP&&:/vshare/Build/project-assignment-service/
-scp  -o StrictHostKeyChecking=no /var/lib/jenkins/jobs/DevOps_Development/workspace/WebApplications/organisation-web-application/target/organisationWebApp.war vagrant@&&DOCKER_HOST_IP&&:/vshare/Build/organisation-web/
+scp  -o StrictHostKeyChecking=no /var/lib/jenkins/jobs/DevOps_Development/workspace/Microservices/employee-service/target/employeeService.jar vagrant@172.16.72.247:/vshare/Build/employee/
+scp  -o StrictHostKeyChecking=no /var/lib/jenkins/jobs/DevOps_Development/workspace/Microservices/project-service/target/projectService.jar vagrant@172.16.72.247:/vshare/Build/project/
+scp  -o StrictHostKeyChecking=no /var/lib/jenkins/jobs/DevOps_Development/workspace/Microservices/project-assignment-service/target/projectAssignmentService.jar vagrant@172.16.72.247:/vshare/Build/project-assignment-service/
+scp  -o StrictHostKeyChecking=no /var/lib/jenkins/jobs/DevOps_Development/workspace/WebApplications/organisation-web-application/target/organisationWebApp.war vagrant@172.16.72.247:/vshare/Build/organisation-web/
 echo ">>>>>>>>>>>>>>>> Transfered Artifact >>>>>>>>>>>>>>>>>>>>"
 echo ".................. Re run the docker container ........................"
-ssh -o StrictHostKeyChecking=no vagrant@&&DOCKER_HOST_IP&& 'cd /vshare/docker/ && ./runAllScripts.sh'
+ssh -o StrictHostKeyChecking=no vagrant@172.16.72.247 'cd /vshare/docker/ && ./runAllScripts.sh'
 echo ".................. Successfully Executed ........................"
 exit
