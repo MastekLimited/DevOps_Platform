64f01ea380aeb1b27cc379fd2f675979e0924522
Krishna9889 <Krishna9889@GRB-RDG1WIN7G02.mastek.com>
2016-01-18 14:01:42 +0000

    Added files.
    Former-commit-id: 790152c6cf40aa12a5e458c6da8e1817ea410b98

--- scripts/elk_setup/centos/logstash-forwarder.sh
+++ scripts/elk_setup/centos/logstash-forwarder.sh
@@ -4,7 +4,7 @@ cp /mnt/gluster/repo/logstash-forwarder.repo  /etc/yum.repos.d
 yum -y install logstash-forwarder
 cp /mnt/gluster/repo/logstash-forwarder.crt /etc/pki/tls/certs
 #OR
-#scp root@172.16.72.246:/mnt/gluster/repo/logstash-forwarder.crt /etc/pki/tls/certs/
+#scp root@&&ELK_HOST_IP&&:/mnt/gluster/repo/logstash-forwarder.crt /etc/pki/tls/certs/
 cp /mnt/gluster/repo/logstash-forwarder.conf  /etc
 service logstash-forwarder restart
 exit
