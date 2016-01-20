09ca8029fda16bbef033300be41b6660f8dcdb57
Krishna Kuntala <kuntala.krishna@mastek.com>
2016-01-20 19:34:45 +0000

    Added device-registration container.
    Former-commit-id: 622b55a49f1cd8b3f2c5dd26bd4968b0d6965f44 [formerly 0c1b4fcf0a71908e83f3e30e0d384af83e125eec] [formerly cb9b2b4468c4656771f7b71085c6ecfcf129201e [formerly 530507c128b5dffde8e6e17c5bbca200a3a3bcbe]] [formerly 8453529fd34411c196107bfc38698b56c370218a] [formerly 0a14b4b677051efa70c4a848a0750371101d5118] [formerly b8c7206885bf89589b0d6266d6dbc8dee2e52b2a [formerly 041b6d5427a10debc4c2324bb81b7d81bb5882cf] [formerly 0a14b4b677051efa70c4a848a0750371101d5118 [formerly 0a14b4b677051efa70c4a848a0750371101d5118 [formerly 0a14b4b677051efa70c4a848a0750371101d5118 [formerly 0bad15b2c088cbb1552ab960ded3a450a28682b5]]]]]
    Former-commit-id: 87bd27186770a7a246b9a1ebc4c96d2f41098395 [formerly e16aeb352079e021fe45d25f60b4724bb2bf5f4d] [formerly 2b70015624414c7d0625f859fd47dda4c8eaa26f [formerly 2b70015624414c7d0625f859fd47dda4c8eaa26f [formerly 2b70015624414c7d0625f859fd47dda4c8eaa26f [formerly b5a211d2438ce267538fe7393a223c5a05b362c7]]]]
    Former-commit-id: 2b70015624414c7d0625f859fd47dda4c8eaa26f
    Former-commit-id: 62e6f0b4f70d761b67cf4c7020768a42417fb22e
    Former-commit-id: 624d38bdc6a1d4a51124f6f6dcf3242a3a3ae8ea [formerly 418a5b1769a32cbb1fc08856a7be3325ae988809]
    Former-commit-id: e2c18236808c2f7976c6d19e8e96c9f8b7d8f70c

--- docker/startupScripts.sh
+++ docker/startupScripts.sh
@@ -2,14 +2,22 @@
 buildAll () {
 echo "build base"
 ./scripts/base/build.sh
+
 echo "build employee"
 ./scripts/employee/build.sh
+
 echo "build project"
 ./scripts/project/build.sh
+
 echo "build project assignment"
 ./scripts/project-assignment/build.sh
+
 echo "build device authentication"
 ./scripts/device-authentication/build.sh
+
+echo "build device registration"
+./scripts/device-registration/build.sh
+
 echo "build organisation web"
 ./scripts/organisation-web/build.sh
 }
