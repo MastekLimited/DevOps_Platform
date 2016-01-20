d6820468b2e4b745f73254dd399d1eb6d1a7406d
Krishna Kuntala <kuntala.krishna@mastek.com>
2016-01-20 17:47:20 +0000

    Fixed problem of device-authentication docker container.
    Former-commit-id: fd5d56f1d3bf976559623c94f5eec3479fc7b759 [formerly 8e4c7063edf7988d0afe4b3f899d049ec3b88917] [formerly ab41fc5a70b74b45a299ea290065f4aa204ed4ec [formerly ab41fc5a70b74b45a299ea290065f4aa204ed4ec [formerly ab41fc5a70b74b45a299ea290065f4aa204ed4ec [formerly ab41fc5a70b74b45a299ea290065f4aa204ed4ec [formerly bcfceb98a65ce4329ea66298d52fb5806f5c644b]]]]]
    Former-commit-id: ab41fc5a70b74b45a299ea290065f4aa204ed4ec
    Former-commit-id: aa4263d695cc2b3ad50fd94dc66d3921abbee394
    Former-commit-id: 9714eb294897620fa5335a09066288dcba6f9d59 [formerly 7345d309ecb111c09bd0d33a2ef8ff1f26af729d]
    Former-commit-id: 09c9052b069b84c8b2c5880ecc290d7c7bd4615f
    Former-commit-id: 0b3a8095a39830471358d49090ff545f9163d0c7

--- docker/scripts/device-authentication/run.sh
+++ docker/scripts/device-authentication/run.sh
@@ -1,1 +1,1 @@
-docker run -p 8095:8095 -p 9095:6556 --name deviceauthentication -h deviceauthentication -v /vshare/docker/config:/home/devops/config -v /vshare/Build/device-authentication/:/usr/share/ deviceauthentication
+docker run -p 8095:8095 -p 9095:6556 --name device-authentication -h device-authentication -v /vshare/docker/config:/home/devops/config -v /vshare/Build/device-authentication/:/usr/share/ device-authentication
