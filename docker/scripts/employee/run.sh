5b2bd7d5f8c11b9d6e6383191524169eb6ec1bc0
Krishna Kuntala <kuntala.krishna@mastek.com>
2016-01-21 20:44:43 +0000

    Updated ports.
    Former-commit-id: 260890734e6f5639ec6c972c4dd28edb9867dde5 [formerly 5cbae973a421d2951adc9e553cc9733229e495ff] [formerly f66b90459ce59ab5e7a99133306e7f19d700edfd [formerly f66b90459ce59ab5e7a99133306e7f19d700edfd [formerly f66b90459ce59ab5e7a99133306e7f19d700edfd [formerly f66b90459ce59ab5e7a99133306e7f19d700edfd [formerly f66b90459ce59ab5e7a99133306e7f19d700edfd [formerly f66b90459ce59ab5e7a99133306e7f19d700edfd [formerly f66b90459ce59ab5e7a99133306e7f19d700edfd [formerly db6c61fdf2086dd7ae3244120e911fdd802da689]]]]]]]]
    Former-commit-id: f66b90459ce59ab5e7a99133306e7f19d700edfd
    Former-commit-id: e5c7c3df9a00c789d94d044b20320816eda167e5
    Former-commit-id: d3763cbc9d557ed46bfabe76698793dd64bced27 [formerly 42f7eb312952345028fb8223ffa175b9b4ca2fb0] [formerly 06e627cf6f027d36b532e3c19f41f8e06e1743df [formerly 5e3479b5b939b386fce4a488f735d57538492437]]
    Former-commit-id: b772eece6b48ef7602513835de29a4a97bec2e54
    Former-commit-id: 4f87fdb14036a0fa33e2844578f34f4134f5d752 [formerly 0ffe6a40c642c67f3e3e638046f9712701007f80] [formerly d42ee95c724149015ffd100e1a3c4c6a09c12329] [formerly 277a3918fa244ec9e1899c2993b5132f606c4e3a [formerly f1296208c759f35b1b0d8cc3fa5ad2e459d041dc] [formerly 8ea802a1b3eaa2ff1c0cee32853052f838bc7833 [formerly 84115cdffad744cbeb3650f459b5d0c2b1715c02]]]
    Former-commit-id: 18fd2863fe0e8725674b845287097c66abae2cfc
    Former-commit-id: 2aeb656dc40f611e328af1225ce6f6fae84e09b4
    Former-commit-id: 44a0161474bfd117490736fefa4ac8427a254659

--- docker/scripts/employee/run.sh
+++ docker/scripts/employee/run.sh
@@ -1,1 +1,1 @@
-docker run -p 8087:8087 -p 9087:6556 --name employee -h employee -v /vshare/docker/config/services:/home/devops/config/services -v /vshare/Build/employee/:/usr/share/ employee
+docker run -p 10010:10010 -p 10011:6556 --name employee -h employee -v /vshare/docker/config/services:/home/devops/config/services -v /vshare/Build/employee/:/usr/share/ employee
