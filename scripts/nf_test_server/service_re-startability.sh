cc125aa371098c5fcbfb765e83254afe409f88a4
unknown <Govind10995@GRB-RDG1WIN7G01.mastek.com>
2016-01-21 11:52:44 +0000

    Updated NF Test Server script
    Former-commit-id: 23b8e96f253b3f2c9fed4df4301e7137ef1a4f7b [formerly 1355a8aa53aeb8a4f79730544ec152bf342ad6a4] [formerly d1c0f68a619cd37025125d82ba93c86971c4ac25 [formerly d1c0f68a619cd37025125d82ba93c86971c4ac25 [formerly 67eb8b1722f439b1ed926b19cd061240e97f0ad8]] [formerly d1c0f68a619cd37025125d82ba93c86971c4ac25 [formerly 67eb8b1722f439b1ed926b19cd061240e97f0ad8] [formerly 67eb8b1722f439b1ed926b19cd061240e97f0ad8 [formerly 67eb8b1722f439b1ed926b19cd061240e97f0ad8 [formerly 0f0cd652bff37c0d6f9ff0fc86830af7aa5449b8]]]]]
    Former-commit-id: d1c0f68a619cd37025125d82ba93c86971c4ac25 [formerly d1c0f68a619cd37025125d82ba93c86971c4ac25 [formerly 67eb8b1722f439b1ed926b19cd061240e97f0ad8]]
    Former-commit-id: cfaf0926525e886356483f70db6208e73d29964b
    Former-commit-id: 62a3261f3fd76a3ee735df1498ce7ce0f459f997 [formerly 0ebf7cadc8bcdc472226593bee322005cf335e79]
    Former-commit-id: a597e791a36628a4c8e819d2b14519a46930bd18
    Former-commit-id: f5e3d80f94ae6c87a7d25d839d424e8404f72dc4

--- scripts/nf_test_server/service_re-startability.sh
+++ scripts/nf_test_server/service_re-startability.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 #sudo service influxdb start
-sudo systemctl stop collectd.service
-sudo systemctl stop influxdb.service
+systemctl stop collectd.service
+systemctl stop influxdb.service
 
 sleep 10
-sudo systemctl start collectd.service
-sudo systemctl start influxdb.service
+systemctl restart collectd.service
+systemctl restart influxdb.service
 
 
-sudo service grafana-server start
-sudo /usr/local/bin/collectd-server start
+service grafana-server start
+/usr/local/bin/collectd-server start
 
 
 sleep 10
-sudo systemctl enable collectd.service
-sudo systemctl enable influxdb.service
-sudo systemctl enable grafana-server
+systemctl enable collectd.service
+systemctl enable influxdb.service
+systemctl enable grafana-server
 
 #sudo service postgresql-9.4 restart
 
@@ -26,6 +26,3 @@ sudo systemctl enable grafana-server
 #sudo java -jar /mnt/gluster/repo/devops/projectService.jar --spring.config.location=file:/mnt/gluster/repo/devops/services.properties,classpath:/application.properties &
 #sleep 10
 #sudo java -jar /mnt/gluster/repo/devops/organisationWebApp.war --spring.config.location=file:/mnt/gluster/repo/devops/services.properties,classpath:/application.properties &
-
-sudo systemctl stop firewalld
-sudo systemctl disable firewalld
