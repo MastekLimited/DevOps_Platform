f7e25735fbd45e0cff0fbce2b2a0e5b54ae64ea7
unknown <Govind10995@GRB-RDG1WIN7G01.mastek.com>
2016-01-21 11:52:44 +0000

    Updated NF Test Server script
    Former-commit-id: bf1b596c8bac937c2d98fc3ccf7fcec658404300 [formerly cc125aa371098c5fcbfb765e83254afe409f88a4] [formerly d4aa948e6985c2c35a8df35c786ac9bc3e94a625 [formerly f5e3d80f94ae6c87a7d25d839d424e8404f72dc4]] [formerly a597e791a36628a4c8e819d2b14519a46930bd18] [formerly 62a3261f3fd76a3ee735df1498ce7ce0f459f997 [formerly 0ebf7cadc8bcdc472226593bee322005cf335e79]] [formerly cfaf0926525e886356483f70db6208e73d29964b] [formerly 67eb8b1722f439b1ed926b19cd061240e97f0ad8] [formerly 23b8e96f253b3f2c9fed4df4301e7137ef1a4f7b [formerly 1355a8aa53aeb8a4f79730544ec152bf342ad6a4] [formerly 67eb8b1722f439b1ed926b19cd061240e97f0ad8 [formerly 67eb8b1722f439b1ed926b19cd061240e97f0ad8 [formerly 67eb8b1722f439b1ed926b19cd061240e97f0ad8 [formerly 67eb8b1722f439b1ed926b19cd061240e97f0ad8 [formerly 67eb8b1722f439b1ed926b19cd061240e97f0ad8 [formerly 0f0cd652bff37c0d6f9ff0fc86830af7aa5449b8]]]]]]]
    Former-commit-id: 40b7e019f1951981b0650c8361fa344add6ae188 [formerly aa49e2983f483ba0ed5e60e01003adfb1c9153ad] [formerly e4d47f0f463259a4b2edbdf57e3de207673bffc2 [formerly e4d47f0f463259a4b2edbdf57e3de207673bffc2 [formerly e4d47f0f463259a4b2edbdf57e3de207673bffc2 [formerly e4d47f0f463259a4b2edbdf57e3de207673bffc2 [formerly e4d47f0f463259a4b2edbdf57e3de207673bffc2 [formerly b20305aa3a950e0ac908c89db4c1a6d1b078e6e3]]]]]]
    Former-commit-id: e4d47f0f463259a4b2edbdf57e3de207673bffc2
    Former-commit-id: 6473dda3d62e7ccd97e90c07946c3ccf201db8aa
    Former-commit-id: d6e8faf39baa26620398c9d3d32356539d1a3b6c [formerly c0b4c7868022193eb93824330d8f68083691a0d2]
    Former-commit-id: 8610834a3d77d505ee5bcd8ff304a30934af7b40
    Former-commit-id: 0646b7a8412d74e45d1c2b6bfd16a00231113cd8 [formerly 48b32879c8ad43feeaca3deb1e6c455c955c2a88]
    Former-commit-id: 9a6e2770b436fcd318187f32ac1b3eae5ad27de4

--- scripts/nf_test_server/influxdb_setup.sh
+++ scripts/nf_test_server/influxdb_setup.sh
@@ -2,23 +2,12 @@
 
 
 # Install influxdb 0.9.5.1-1.x86_64 RPM
-
+cd /opt
 echo ...........................Installing influxdb 0.9.5 RPM............................
-sudo wget http://influxdb.s3.amazonaws.com/influxdb-0.9.5.1-1.x86_64.rpm
-sudo yum install -y influxdb-0.9.5.1-1.x86_64.rpm
-sudo sudo cp /mnt/gluster/repo/influxdb.conf /etc/influxdb/
-sudo systemctl start influxdb.service
-
-# Install influxdb 0.8.9-1.x86_64 RPM
-#echo ...........................Installing influxdb-0.9.5 RPM............................
-#sudo wget http://get.influxdb.org.s3.amazonaws.com/influxdb-0.8.9-1.x86_64.rpm
-#sudo yum localinstall -y influxdb-0.8.9-1.x86_64.rpm
-#sudo stat /usr/lib64/libbz2.so.1
-#sudo ln -s /usr/lib64/libbz2.so.1.0.6 /usr/lib64/libbz2.so.1.0
-#sudo ldd /usr/bin/influxdb
-#sudo /etc/init.d/influxdb start
-
-
+wget http://influxdb.s3.amazonaws.com/influxdb-0.9.5.1-1.x86_64.rpm
+yum install -y influxdb-0.9.5.1-1.x86_64.rpm
+cp /mnt/gluster/repo/influxdb.conf /etc/influxdb/
+systemctl start influxdb.service
 sleep 10 
-sudo curl -G 'http://localhost:8086/query' --data-urlencode "q=CREATE DATABASE jmeter"
-sudo curl -G 'http://localhost:8086/query' --data-urlencode "q=CREATE DATABASE collectd"
+curl -G 'http://localhost:8086/query' --data-urlencode "q=CREATE DATABASE jmeter"
+curl -G 'http://localhost:8086/query' --data-urlencode "q=CREATE DATABASE collectd"
