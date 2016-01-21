48b32879c8ad43feeaca3deb1e6c455c955c2a88
unknown <Govind10995@GRB-RDG1WIN7G01.mastek.com>
2016-01-21 11:52:44 +0000

    Updated NF Test Server script
    Former-commit-id: f5e3d80f94ae6c87a7d25d839d424e8404f72dc4 [formerly a597e791a36628a4c8e819d2b14519a46930bd18] [formerly 62a3261f3fd76a3ee735df1498ce7ce0f459f997 [formerly 0ebf7cadc8bcdc472226593bee322005cf335e79]] [formerly cfaf0926525e886356483f70db6208e73d29964b] [formerly 67eb8b1722f439b1ed926b19cd061240e97f0ad8] [formerly 23b8e96f253b3f2c9fed4df4301e7137ef1a4f7b [formerly 1355a8aa53aeb8a4f79730544ec152bf342ad6a4] [formerly 67eb8b1722f439b1ed926b19cd061240e97f0ad8 [formerly 67eb8b1722f439b1ed926b19cd061240e97f0ad8 [formerly 67eb8b1722f439b1ed926b19cd061240e97f0ad8 [formerly 0f0cd652bff37c0d6f9ff0fc86830af7aa5449b8]]]]]
    Former-commit-id: 40b7e019f1951981b0650c8361fa344add6ae188 [formerly aa49e2983f483ba0ed5e60e01003adfb1c9153ad] [formerly e4d47f0f463259a4b2edbdf57e3de207673bffc2 [formerly e4d47f0f463259a4b2edbdf57e3de207673bffc2 [formerly e4d47f0f463259a4b2edbdf57e3de207673bffc2 [formerly b20305aa3a950e0ac908c89db4c1a6d1b078e6e3]]]]
    Former-commit-id: e4d47f0f463259a4b2edbdf57e3de207673bffc2
    Former-commit-id: 6473dda3d62e7ccd97e90c07946c3ccf201db8aa
    Former-commit-id: d6e8faf39baa26620398c9d3d32356539d1a3b6c [formerly c0b4c7868022193eb93824330d8f68083691a0d2]
    Former-commit-id: 8610834a3d77d505ee5bcd8ff304a30934af7b40

--- scripts/nf_test_server/devops_apps/service_start.sh
+++
@@ -1,3 +0,0 @@
-sudo java -jar employeeService.jar --spring.config.location=file:services.properties,classpath:/application.properties &
-sudo java -jar projectService.jar --spring.config.location=file:services.properties,classpath:/application.properties &
-sudo java -jar organisationWebApp.war --spring.config.location=file:services.properties,classpath:/application.properties &
