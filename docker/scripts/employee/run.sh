98a78714bd61170ad27e485787cdbd81d5fee344
Krishna Kuntala <kuntala.krishna@mastek.com>
2016-01-21 16:46:58 +0000

    Modified services.properties file path.
    Former-commit-id: 3379aad659d97c3230cdb533e3168c3fe0c52e5a [formerly 853b0ab2cec75aa0dbcb7dc56a5d10d5bb989e66] [formerly 4a61656490a9c23417f7db8272cbde9ddccdbf35 [formerly 4a61656490a9c23417f7db8272cbde9ddccdbf35 [formerly 545599ca4d6dc4f865db935c4a3ef0abc099db52]] [formerly 4a61656490a9c23417f7db8272cbde9ddccdbf35 [formerly 545599ca4d6dc4f865db935c4a3ef0abc099db52] [formerly 545599ca4d6dc4f865db935c4a3ef0abc099db52 [formerly 545599ca4d6dc4f865db935c4a3ef0abc099db52 [formerly 809fd04c3eb7148608390be1653ae7e3962159dc]]]]]
    Former-commit-id: 4a61656490a9c23417f7db8272cbde9ddccdbf35 [formerly 4a61656490a9c23417f7db8272cbde9ddccdbf35 [formerly 545599ca4d6dc4f865db935c4a3ef0abc099db52]]
    Former-commit-id: 0dd06e6fc3da79fd8fa1e1676c9c17799a24e22a
    Former-commit-id: 0d418382498c875e0ab49f41922b37a2a4018a13 [formerly e66681d1fc2c245ea2b3c930bfd99e199596c6a7]
    Former-commit-id: efe9b763e6013db8c83f015908ba50847311c877
    Former-commit-id: b78d936fa359acec507f3906a63f5ad58e600737

--- docker/scripts/employee/run.sh
+++ docker/scripts/employee/run.sh
@@ -1,1 +1,1 @@
-docker run -p 8087:8087 -p 9087:6556 --name employee -h employee -v /vshare/docker/config:/home/devops/config -v /vshare/Build/employee/:/usr/share/ employee
+docker run -p 8087:8087 -p 9087:6556 --name employee -h employee -v /vshare/docker/config/services:/home/devops/config/services -v /vshare/Build/employee/:/usr/share/ employee
