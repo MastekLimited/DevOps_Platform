f36c470e7f337b810cf0c242c5fcaba50c334948
Krishna Kuntala <kuntala.krishna@mastek.com>
2016-01-21 20:44:43 +0000

    Updated ports.
    Former-commit-id: db6c61fdf2086dd7ae3244120e911fdd802da689

--- docker/scripts/project-assignment/run.sh
+++ docker/scripts/project-assignment/run.sh
@@ -1,1 +1,1 @@
-docker run -p 8092:8092 -p 9092:6556 --name project-assignment -h project-assignment -v /vshare/docker/config/services:/home/devops/config/services -v /vshare/Build/project-assignment/:/usr/share/ project-assignment
+docker run -p 10030:10030 -p 10031:6556 --name project-assignment -h project-assignment -v /vshare/docker/config/services:/home/devops/config/services -v /vshare/Build/project-assignment/:/usr/share/ project-assignment
