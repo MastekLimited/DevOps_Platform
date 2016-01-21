e4d47f0f463259a4b2edbdf57e3de207673bffc2
unknown <Govind10995@GRB-RDG1WIN7G01.mastek.com>
2016-01-21 11:52:44 +0000

    Updated NF Test Server script
    Former-commit-id: 67eb8b1722f439b1ed926b19cd061240e97f0ad8 [formerly 0f0cd652bff37c0d6f9ff0fc86830af7aa5449b8]
    Former-commit-id: b20305aa3a950e0ac908c89db4c1a6d1b078e6e3

--- scripts/nf_test_server/dashborad_setup.sh
+++ scripts/nf_test_server/dashborad_setup.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-# Grafana2 getdash scripted dashboard install script.
-
-#sudo git clone --depth=1 https://github.com/anryko/grafana-influx-dashboard.git
-#cd grafana-influx-dashboard
-sudo ./install.sh /usr/share/grafana
+cd /mnt/gluter/repo
+./install.sh /usr/share/grafana
 echo "Install finished."
 
 echo "Re-starting grafana server."
-sudo service grafana-server start
+service grafana-server start
