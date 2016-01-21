a9b05b201cc2370a42d083cb5efbba780f9812dd
unknown <Govind10995@GRB-RDG1WIN7G01.mastek.com>
2016-01-21 11:52:44 +0000

    Updated NF Test Server script
    Former-commit-id: 23b8e96f253b3f2c9fed4df4301e7137ef1a4f7b [formerly 1355a8aa53aeb8a4f79730544ec152bf342ad6a4] [formerly d1c0f68a619cd37025125d82ba93c86971c4ac25 [formerly d1c0f68a619cd37025125d82ba93c86971c4ac25 [formerly 67eb8b1722f439b1ed926b19cd061240e97f0ad8]] [formerly d1c0f68a619cd37025125d82ba93c86971c4ac25 [formerly 67eb8b1722f439b1ed926b19cd061240e97f0ad8] [formerly 67eb8b1722f439b1ed926b19cd061240e97f0ad8 [formerly 67eb8b1722f439b1ed926b19cd061240e97f0ad8 [formerly 67eb8b1722f439b1ed926b19cd061240e97f0ad8 [formerly 67eb8b1722f439b1ed926b19cd061240e97f0ad8 [formerly 0f0cd652bff37c0d6f9ff0fc86830af7aa5449b8]]]]]]]
    Former-commit-id: d1c0f68a619cd37025125d82ba93c86971c4ac25 [formerly d1c0f68a619cd37025125d82ba93c86971c4ac25 [formerly 67eb8b1722f439b1ed926b19cd061240e97f0ad8]]
    Former-commit-id: cfaf0926525e886356483f70db6208e73d29964b
    Former-commit-id: a8198018e4e6554475d32a49f221c514b38124bc [formerly 62a3261f3fd76a3ee735df1498ce7ce0f459f997] [formerly c6dea569a7ab2e514131dc74ec958ede6b50c55e [formerly 0ebf7cadc8bcdc472226593bee322005cf335e79]]
    Former-commit-id: a597e791a36628a4c8e819d2b14519a46930bd18
    Former-commit-id: 3fa2543c2fc2bef761b726e7ce97677b52ef4231 [formerly 80660431fe758548b2e7773823d8966815a10076] [formerly cc125aa371098c5fcbfb765e83254afe409f88a4 [formerly f5e3d80f94ae6c87a7d25d839d424e8404f72dc4]]
    Former-commit-id: d3a609cd24419f56ec1af6597de23110a9649eac
    Former-commit-id: e49b57feaf3c66ba2cde115468b7f260eb658e3a

--- scripts/nf_test_server/collectd_setup.sh
+++ scripts/nf_test_server/collectd_setup.sh
@@ -1,64 +1,64 @@
 #!/bin/bash
 # Perform installation as root
 # Install prereqs
-
+cd /opt
 echo ...........................Install prereqs............................
 #sudo yum -y install libcurl libcurl-devel rrdtool rrdtool-devel rrdtool-prel libgcrypt-devel libyajl libyajl-devel gcc make gcc-c++
-sudo yum install -y rrdtool rrdtool-devel rrdtool-perl perl-HTML-Parser perl-JSON perl-CPAN perl-devel gcc make gcc-c++
+yum install -y rrdtool rrdtool-devel rrdtool-perl perl-HTML-Parser perl-JSON perl-CPAN perl-devel gcc make gcc-c++
 
 # Enable the EPEL
 echo ...........................Install EPEL............................
 #sudo yum -y install http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
-sudo yum install -y epel-release
+yum install -y epel-release
 
 # Get Collectd, untar it, make it and install
 echo ...........................Get Collectd TAR file............................
-sudo wget http://collectd.org/files/collectd-5.4.0.tar.gz
+wget http://collectd.org/files/collectd-5.4.0.tar.gz
 echo ...........................Untar Collectd TAR file............................
-sudo tar zxvf collectd-5.4.0.tar.gz
+tar zxvf collectd-5.4.0.tar.gz
 
 cd collectd-5.4.0
 
 echo ...........................Executed configure file............................
-sudo ./configure --prefix=/usr --sysconfdir=/etc --localstatedir=/var --libdir=/usr/lib64 --mandir=/usr/share/man --enable-all-plugins
+./configure --prefix=/usr --sysconfdir=/etc --localstatedir=/var --libdir=/usr/lib64 --mandir=/usr/share/man --enable-all-plugins
 
 #echo ...........................Executed MAKE. command...........................
 #sudo make
 
 echo ...........................Executed MAKE Install command............................
-sudo make all install
+make all install
 
 # Copy the default init.d script
 echo ...........................Copy the default init.d script............................
-sudo cp /home/vagrant/collectd-5.4.0/contrib/redhat/init.d-collectd /etc/init.d/collectd
+cp /opt/collectd-5.4.0/contrib/redhat/init.d-collectd /etc/init.d/collectd
 
 # Set the correct permissions
 echo ...........................Set the correct permissions............................
-sudo chmod +x /etc/init.d/collectd
+chmod +x /etc/init.d/collectd
 
 # Copying collectd service file and creating soft link 
 echo ...........................Copying collectd service file and creating soft link............................
-sudo cp /mnt/gluster/repo/collectd.service /usr/lib/systemd/system/
-sudo ln -sf /usr/lib/systemd/system/collectd.service /etc/systemd/system/multi-user.target.wants/collectd.service
+cp /mnt/gluster/repo/collectd.service /usr/lib/systemd/system/
+ln -sf /usr/lib/systemd/system/collectd.service /etc/systemd/system/multi-user.target.wants/collectd.service
 
 # Start the deamon
 echo ...........................Start the deamon............................
 #sudo service collectd start
-sudo systemctl start collectd.service
-sudo systemctl enable collectd.service
+systemctl start collectd.service
+systemctl enable collectd.service
 
 # Install Collectd Web
 echo ...........................Install Collectd Web............................
 sudo yum install -y git
 #sudo yum install -y rrdtool rrdtool-devel rrdtool-perl perl-HTML-Parser perl-JSON
- cd /usr/local/
-sudo git clone https://github.com/httpdss/collectd-web.git
- cd /usr/local/collectd-web
-sudo chmod +x cgi-bin/graphdefs.cgi
-sudo ./runserver.py &
-sudo cp /mnt/gluster/repo/collectd.conf /etc/collectd.conf
-sudo cp /mnt/gluster/repo/collectd-server /usr/local/bin/collectd-server
-sudo chmod +x /usr/local/bin/collectd-server
+cd /usr/local/
+git clone https://github.com/httpdss/collectd-web.git
+cd /usr/local/collectd-web
+chmod +x cgi-bin/graphdefs.cgi
+./runserver.py &
+cp /mnt/gluster/repo/collectd.conf /etc/collectd.conf
+cp /mnt/gluster/repo/collectd-server /usr/local/bin/collectd-server
+chmod +x /usr/local/bin/collectd-server
 
-sudo /usr/local/bin/collectd-server start 
+/usr/local/bin/collectd-server start 
 
