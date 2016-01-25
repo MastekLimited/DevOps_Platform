1dccb927e3af054ab04741fbe390e6acabe97ded
unknown <Govind10995@GRB-RDG1WIN7G01.mastek.com>
2016-01-25 14:14:31 +0000

    NF Server scripts updated
    Former-commit-id: 9211cfc4affa39192e5326d8ccc03138c8ea5b0f [formerly 23595dbe00c472f9fbd2d532c5d90742bfc79d7c] [formerly 148d29c745f538eb774a479cef6f9a1fd725c15c [formerly 148d29c745f538eb774a479cef6f9a1fd725c15c [formerly 148d29c745f538eb774a479cef6f9a1fd725c15c [formerly 148d29c745f538eb774a479cef6f9a1fd725c15c [formerly 148d29c745f538eb774a479cef6f9a1fd725c15c [formerly 148d29c745f538eb774a479cef6f9a1fd725c15c [formerly c8d2e52dc39791cb9f78cc97fed3403c50e1304a]]]]]]]
    Former-commit-id: 148d29c745f538eb774a479cef6f9a1fd725c15c
    Former-commit-id: 0c24215d69782f2821f912eafbe01f650f91b459
    Former-commit-id: e662059ea15b8a26bbc17d2c2ce56be257acf682 [formerly 8197a53f82ae2fa80e4d6093d2bb9895b3f8503e]
    Former-commit-id: 9882fbb211e182946adfd7f4c5d248005ec6703a
    Former-commit-id: 72bd37167334c0795c9726c241cce67d99cd835f [formerly ee11819a13cab2d0335713ca89eb23d50a903e76] [formerly 0f1c130d33d176d96699557b51079b88cdb133e7 [formerly a71c5bbbb5d5a757c77cd7444dff4a274870bf4c]]
    Former-commit-id: 7a2c7e04b8f749a19d408663f63497af3cf153ba
    Former-commit-id: cd9db66893cb2651cdf72e1d1cf1138ee6f6e3de

--- scripts/nf_test_server/dashborad_setup.sh
+++ scripts/nf_test_server/dashborad_setup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-cd /mnt/gluter/repo
+cd /mnt/gluster/repo/
 ./install.sh /usr/share/grafana
 echo "Install finished."
 
-echo "Re-starting grafana server."
+echo "Re-Starting grafana server."
 service grafana-server start
+
+echo "Re-Starting collectd service."
+systemctl restart collectd.service
