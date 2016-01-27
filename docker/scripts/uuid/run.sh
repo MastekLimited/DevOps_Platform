e7d96d0572b077fce9c96b51270a18c752265f5e
Krishna9889 <Krishna9889@GRB-RDG1WIN7G02.mastek.com>
2016-01-27 12:03:00 +0000

    Solved problem of directory mounting on docker. Vagrantfile refer local centos image.
    Former-commit-id: 369380c562ca3ee437686d6edef7e5f77ce1bf4d

--- docker/scripts/uuid/run.sh
+++ docker/scripts/uuid/run.sh
@@ -1,1 +1,1 @@
-docker run -p 10000:10000 -p 10001:6556 --name uuid -h uuid -v /vshare/docker/config/services:/home/devops/config/services -v /vshare/Build/uuid/:/usr/share/ uuid
+docker run -p 10000:10000 -p 10001:6556 --name uuid -h uuid -v /mnt/devops/config/services:/home/devops/config/services -v /mnt/devops/Build/uuid/:/usr/share/ uuid
