5ab10a0f7b7f3fb094318c917bfcd7f577be672e
Krishna Kuntala <krishna.kuntala@mastek.com>
2016-02-05 16:23:41 +0000

    dos2unix
    Former-commit-id: a79d396b37ec2b14170b5d4d5f0b499e49b89fcd [formerly bdb8b2858cf2f37fce8797b4edc858159552454e] [formerly bda05e4c3f056c25aad4ee2e297a8108faf57671] [formerly 9cabc56fac5095512fb1ffb54bd9ba301bd20ee5 [formerly 39ef545f30eee95ac586e13f8edff1115ccf6cf5]] [formerly b2bd65cbea82c59636aa1d50281e41d0c3d8b61d] [formerly e357886eceacf157e9bbe076ae1af1ac8876f6ef] [formerly d588c2e214f4fdcbd5552407c3d296774c16d8a0 [formerly 83a0b8bc556f89749d62bded6f0309121d2bea39] [formerly e357886eceacf157e9bbe076ae1af1ac8876f6ef [formerly e357886eceacf157e9bbe076ae1af1ac8876f6ef [formerly e357886eceacf157e9bbe076ae1af1ac8876f6ef [formerly e357886eceacf157e9bbe076ae1af1ac8876f6ef [formerly 1c459fece80d599fc43c0263d67c7e5519fe3b78]]]]]]
    Former-commit-id: bfc613c5235c92b715338827f08582cb48f7533f [formerly 5bf6bed598fd2c1e502528237489d9b6a0b24314] [formerly 45544c4cc4c87c99cf13e7de24ea3b9b42f90f50 [formerly 885f9451d2488bd3e10c80e50a22f4eb1cbf6f3f] [formerly 45544c4cc4c87c99cf13e7de24ea3b9b42f90f50 [formerly 885f9451d2488bd3e10c80e50a22f4eb1cbf6f3f] [formerly 45544c4cc4c87c99cf13e7de24ea3b9b42f90f50 [formerly 885f9451d2488bd3e10c80e50a22f4eb1cbf6f3f] [formerly 885f9451d2488bd3e10c80e50a22f4eb1cbf6f3f [formerly a5b33a3b6af23401a6a6e160dfa0f7d4bd36f1d9]]]]]
    Former-commit-id: 46b2d996f949b742eb61f6b62c4405c226f0d56b [formerly 45544c4cc4c87c99cf13e7de24ea3b9b42f90f50 [formerly 885f9451d2488bd3e10c80e50a22f4eb1cbf6f3f]]
    Former-commit-id: 227231b2d554a9767eaeeebce4034369281b1397
    Former-commit-id: 3b9dc1b51ef68e49cc785a9f77566279ef794bd9 [formerly 92e194f87a01ad556c8ea8dcb0bc89856cb33a59]
    Former-commit-id: 5145da7786d1ba55e740e349a8212bcd267d5de0
    Former-commit-id: 7d0849e509a6697ebe6cb09134f907da29c875f7

--- removeAllVagrantEnv.sh
+++ removeAllVagrantEnv.sh
@@ -3,7 +3,7 @@ echo ""
 echo "Vagrant instances on this machine before destroying:"
 echo ""
 vagrant global-status
-vagrant global-status | for vagrantMachineId in $(awk '/running/{print $1}') 
+vagrant global-status | for vagrantMachineId in $(awk '/running/{print $1}')
 do echo "Destroying vagrant machine:-  $vagrantMachineId"
 vagrant destroy -f  $vagrantMachineId
 done;
