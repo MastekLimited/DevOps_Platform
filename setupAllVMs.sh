ce4c61027eeeb5dd69b7d9cffaafdddbb361e174
Krishna Kuntala <krishna.kuntala@mastek.com>
2016-02-05 12:31:31 +0000

    Commented NF server execution.
    Former-commit-id: a2181b4c6ee476d32f12f910b95fc933dc6157a5 [formerly 9dccf7af401818d3ec429c91c7e7043b5af7e9dc] [formerly 9312829fb703c3bd29baa213948e5a74b77e6182 [formerly 0b919b1290c43bb3c61e638404c20539ab609177]] [formerly 0de9d7b1ecf1f243358c74dda9bbedf898fd3301] [formerly 044abf00ed432be41ddd4da3023e9ddcd6d9c6ed] [formerly fee19aae4ad1ea7740db1d2fc6474ed40e0c98e3 [formerly 78ad3ed9bc04845d4f23ede543357562d0fdce55] [formerly 044abf00ed432be41ddd4da3023e9ddcd6d9c6ed [formerly 044abf00ed432be41ddd4da3023e9ddcd6d9c6ed [formerly 044abf00ed432be41ddd4da3023e9ddcd6d9c6ed [formerly 0ab5341f818d55bda7f154f836ba4423ec5f8ebc]]]]]
    Former-commit-id: 5a336e9a5249e747b121d6fff3ea22babc3eebf5 [formerly 76a67d91f419c7847216a6dbe85683b04e27d683] [formerly 22d418e49cb7d49d636eb12e70c9a911efe1f3ed [formerly 22d418e49cb7d49d636eb12e70c9a911efe1f3ed [formerly 979fcb1ee5644acfb01596c8d23f0af29ced8dfa]] [formerly 22d418e49cb7d49d636eb12e70c9a911efe1f3ed [formerly 979fcb1ee5644acfb01596c8d23f0af29ced8dfa] [formerly 979fcb1ee5644acfb01596c8d23f0af29ced8dfa [formerly e8e9529df0ae25a46e7e0a3c3a72d8c9d1cd0630]]]]
    Former-commit-id: 22d418e49cb7d49d636eb12e70c9a911efe1f3ed [formerly 22d418e49cb7d49d636eb12e70c9a911efe1f3ed [formerly 979fcb1ee5644acfb01596c8d23f0af29ced8dfa]]
    Former-commit-id: 63cbc4939daec26f5c71447ab66f4d56e29e0842
    Former-commit-id: b7e8c83238b9ccbdcc875d15b35c2f64adfa380e [formerly d6bdbcfe53855349ffefeec49fbf590ab8b42a06]
    Former-commit-id: 40330f955f95b1631f849207f67fabbff4aaf774

--- setupAllVMs.sh
+++ setupAllVMs.sh
@@ -114,9 +114,9 @@ docker_vm_start_time=`date +%s`
 startCleanVirtualBox vagrant/docker/ ${IPMAP["DOCKER_HOST_IP"]}
 docker_vm_end_time=`date +%s`
 
-nf_vm_start_time=`date +%s`
-startCleanVirtualBox vagrant/nf_test_server/ ${IPMAP["NF_HOST_IP"]}
-nf_vm_end_time=`date +%s`
+#nf_vm_start_time=`date +%s`
+#startCleanVirtualBox vagrant/nf_test_server/ ${IPMAP["NF_HOST_IP"]}
+#nf_vm_end_time=`date +%s`
 
 #git reset --hard
 #git clean -f
@@ -131,6 +131,6 @@ printExecutionTime `expr $elk_vm_end_time - $elk_vm_start_time` "elk setup time"
 printExecutionTime `expr $postgres_vm_end_time - $postgres_vm_start_time` "postgres setup time"
 printExecutionTime `expr $omd_vm_end_time - $omd_vm_start_time` "omd setup time"
 printExecutionTime `expr $docker_vm_end_time - $docker_vm_start_time` "docker setup time"
-printExecutionTime `expr $nf_vm_end_time - $nf_vm_start_time` "nf server setup time"
+#printExecutionTime `expr $nf_vm_end_time - $nf_vm_start_time` "nf server setup time"
 printExecutionTime `expr $all_vms_end_time - $all_vms_start_time` "Total execution time"
 echo "----------------------------------------------------------------------------------------------"
