53864ade93c66df898ab4556f2a2bd856ee58695
Krishna Kuntala <krishna.kuntala@mastek.com>
2016-02-08 21:51:08 +0000

    Update file paths.
    Former-commit-id: f4e64e3c1a82f8ca4eab5645f9d2e1f72b721f50 [formerly 0d5d3a02baae3226ef2c5373b6e3ee7c5ab72bdd] [formerly 74b6cd8e941045df716e47a0a9b45edc287fab24] [formerly 28c9a486305208498b5f71deb2800801c04f3330 [formerly 21069912d7a8f4fb7cc5100f02d78c248de12421]] [formerly e60b316511581cf1b2467b2a6e2b6aea43a0147b] [formerly 4c4cf69dbde7092c88022941b583a227a4fffafb [formerly d5cb777723ebb929af8cb8a49eaa7be2764fbfb6]] [formerly 76a446cdaed69e3a31759cb867739b10a92eb533 [formerly a7e6776a3aac6ed5abad1558fa31f20fd19c11ae] [formerly d5cb777723ebb929af8cb8a49eaa7be2764fbfb6 [formerly d5cb777723ebb929af8cb8a49eaa7be2764fbfb6 [formerly d5cb777723ebb929af8cb8a49eaa7be2764fbfb6 [formerly d5cb777723ebb929af8cb8a49eaa7be2764fbfb6 [formerly d47db508ed9d7e4f6dfeb5a81b670c0ecf7148f9]]]]]]
    Former-commit-id: 09bd21ee62521cd269794e4e5dc80229be31624c [formerly 6be20f32209a82f558be1ce67a0daac20ddb913f] [formerly 1ea4fe2c0933ce4f3e0b7434893a35120cf4c180 [formerly 1ea4fe2c0933ce4f3e0b7434893a35120cf4c180 [formerly 1ea4fe2c0933ce4f3e0b7434893a35120cf4c180 [formerly 1ea4fe2c0933ce4f3e0b7434893a35120cf4c180 [formerly 9f9bf77ce81f62749466ee027425234872ad6e4a]]]]]
    Former-commit-id: 1ea4fe2c0933ce4f3e0b7434893a35120cf4c180
    Former-commit-id: ee9350f278156a5d9b65702e3c91ff5b33687b31
    Former-commit-id: 9b7cd576095ed2fd93e10635aa823cee9dc02722 [formerly 8f029a007c5cda3c651aa906fd0f087b7ab9cc7d]
    Former-commit-id: d5e6602a4076c0f6120c5898739805212755a91a
    Former-commit-id: 4914eb93dd2260e2d50d642347ea722273d3b0be

--- scripts/omd_seup/agent/setup-centos.sh
+++ scripts/omd_seup/agent/setup-centos.sh
@@ -5,7 +5,7 @@ mkdir -p $INSTALLATION_DIRECTORY
 cd $INSTALLATION_DIRECTORY
 
 echo ...........................Installing check mk agent...........................
-if [ -f "/vshare/base-images/jdk/jdk-8u45-linux-x64.rpm" ]; then
+if [ -f "/vshare/base-images/check_mk/agent/check_mk-agent-1.2.4p5-1.noarch.rpm" ]; then
 	yum install -y /vshare/base-images/check_mk/agent/check_mk-agent-1.2.4p5-1.noarch.rpm
 else
 	wget http://mathias-kettner.de/download/check_mk-agent-1.2.4p5-1.noarch.rpm
