929c91d6c7e824c57c303dbc8400ff5870b6d972
Krishna Kuntala <krishna.kuntala@mastek.com>
2016-02-08 16:57:44 +0000

    Intermidiate checkin.
    Former-commit-id: bc521b6f996c2d8558d57148ae96ae512929927a [formerly f24f7d9f510f3cfa72a4e15d7845d7fdb127e2b1] [formerly f73287aa815d1dead14b7318f75edfe874bc235d [formerly 244a7c7b0760c31d91624a13302db5fa2c5271ac]] [formerly 4c90c32dc410ee9d4a1b44295e495a266cf4a48f] [formerly 0c8ff87df03f06076603d9c9d32a21bd164f4bb0 [formerly 579a473c9896cd3f8005b445147c11a8ccbe81a0]] [formerly ada6b08a4f435f56857db0b4b8d1a203afe7276a] [formerly 09d845031fbeb71a75f99a727e05e82697ef32e5] [formerly 04bac8824a5121eea50fb74fbd1746f786a4fe34 [formerly 799e7b9b66e0930ec72cd40fc6a2c244a0bbd41e] [formerly 09d845031fbeb71a75f99a727e05e82697ef32e5 [formerly 09d845031fbeb71a75f99a727e05e82697ef32e5 [formerly 09d845031fbeb71a75f99a727e05e82697ef32e5 [formerly 09d845031fbeb71a75f99a727e05e82697ef32e5 [formerly 09d845031fbeb71a75f99a727e05e82697ef32e5 [formerly 18f6de679f5f4d982c5882428aa264e8605fbbff]]]]]]]
    Former-commit-id: 12a93a215235091e37d805b3a9995db25cac25b1 [formerly c3dc9453ca49f64b874e850b185cd91f8d142d8c] [formerly 49dea92922d2b307b828af993c7f1aa4dcb19667 [formerly b3c13438933b6f8923c16023e6b71fd7f10b568d] [formerly b3c13438933b6f8923c16023e6b71fd7f10b568d [formerly b3c13438933b6f8923c16023e6b71fd7f10b568d [formerly b3c13438933b6f8923c16023e6b71fd7f10b568d [formerly b3c13438933b6f8923c16023e6b71fd7f10b568d [formerly 83c2cf961d7c901d0c665b7b595e173757df2b9d]]]]]]
    Former-commit-id: 49dea92922d2b307b828af993c7f1aa4dcb19667 [formerly b3c13438933b6f8923c16023e6b71fd7f10b568d]
    Former-commit-id: a81d2daea48aa849c733c76a65a75c586de08fcd
    Former-commit-id: 07ad627db430fe941a16096f8cfb5783f390922d [formerly a3e2e067372d51ac9b12a2fa581d214979ba825d]
    Former-commit-id: 4f462435a00a5c7d4b269e9afb056cb1ee979d8c
    Former-commit-id: 0078a6b487f62512f750889e740d2fd8aeb72a20 [formerly d7aa819f3f22d3ee77cb7327416d93834d1f84a0]
    Former-commit-id: 3a6e85635a3982dd8d385495af2d7eea06646860

--- scripts/vm-initial-setup/basicInstallations.sh
+++ scripts/vm-initial-setup/basicInstallations.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 #This script will install basic software on the vm
 
-echo "Installing Java"
-yum install -y java
-
 #echo "Installing APT"
 #wget http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm
-	
+
 #rpm -i rpmforge-release-0.5.*.rpm
 
 #yum install apt
 
 #sudo apt-get update
-
 
 echo "Installing Net tools (ifconfig)"
 yum install -y net-tools
