4f2432e30135bf956279f2cd5a7a1410f5067054
Krishna Kuntala <krishna.kuntala@mastek.com>
2016-02-09 14:18:39 +0000

    Restructured OMD agent and server installation directories. Added checks for local packages if available.
    Former-commit-id: 9714178031cde7b7988e7880fb783ea54bf6ecab [formerly f4c2d1ee545f9a3491f9df8bda02ecd14d6f1812] [formerly f55fd52466c44c03bdd85306de64feeb3964b536 [formerly f55fd52466c44c03bdd85306de64feeb3964b536 [formerly f55fd52466c44c03bdd85306de64feeb3964b536 [formerly f55fd52466c44c03bdd85306de64feeb3964b536 [formerly 4cae971438632baf6ba36fbb88ddeba0158368b7]]]]]
    Former-commit-id: f55fd52466c44c03bdd85306de64feeb3964b536
    Former-commit-id: 319fc2e5f125cbd45120f549f1bc3bab9035edb0
    Former-commit-id: 5d8e9d42a081d6729bae4811a6e827ab5d81890b [formerly 0f8f0fcaf9c86a8fb6de32200bdf3451059a86d8]
    Former-commit-id: c2ee007b0923e385c8928bc2ec65b14298d1104b
    Former-commit-id: 8374629732538ed0366ef837b43db50f07e4a876

--- scripts/omd_seup/server/setup-centos.sh
+++ scripts/omd_seup/server/setup-centos.sh
@@ -1,46 +1,104 @@
-INSTALL_DIR=/opt/omd
-mkdir -p $INSTALL_DIR
-cd $INSTALL_DIR
+INSTALLATION_DIRECTORY=/opt/omd
+mkdir -p $INSTALLATION_DIRECTORY
+cd $INSTALLATION_DIRECTORY
+chmod 777 *
 
 yum remove MySQL-*
-yum -y install mariadb-server mariadb
+
+echo ...........................Installing mariadb...........................
+if [ -f "/vshare/base-images/check_mk/server/mariadb-server-5.5.44-2.el7.centos.x86_64.rpm" ]; then
+	yum install -y /vshare/base-images/check_mk/server/mariadb-server-5.5.44-2.el7.centos.x86_64.rpm mariadb
+else
+	yum -y install mariadb-server mariadb
+fi
+
+echo ...........................Starting mariadb...........................
 systemctl start mariadb
 systemctl enable mariadb
 systemctl start mariadb
 
-wget  http://pkgs.repoforge.org/perl-Net-SNMP/perl-Net-SNMP-5.2.0-1.2.el6.rf.noarch.rpm
-wget  http://pkgs.repoforge.org/libmcrypt/libmcrypt-2.5.7-1.2.el6.rf.x86_64.rpm
-wget  http://pkgs.repoforge.org/fping/fping-3.10-1.el6.rf.x86_64.rpm
-wget  http://pkgs.repoforge.org/radiusclient-ng/radiusclient-ng-0.5.6-5.el6.rf.x86_64.rpm
-wget http://files.omdistro.org/releases/centos_rhel/omd-1.20.rhel7.x86_64.rpm
-wget http://mathias-kettner.de/download/check_mk-agent-1.2.4p5-1.noarch.rpm
-chmod 777 *
+echo ...........................Installing SNMP...........................
+if [ -f "/vshare/base-images/check_mk/server/perl-Net-SNMP-5.2.0-1.2.el6.rf.noarch.rpm" ]; then
+	echo "perl-Net-SNMP-5.2.0-1.2.el6.rf.noarch found locally"
+	cp /vshare/base-images/check_mk/server/perl-Net-SNMP-5.2.0-1.2.el6.rf.noarch.rpm .
+else
+	wget  http://pkgs.repoforge.org/perl-Net-SNMP/perl-Net-SNMP-5.2.0-1.2.el6.rf.noarch.rpm
+fi
 yum -y install perl-Net-SNMP-5.2.0-1.2.el6.rf.noarch.rpm
+
+echo ...........................Installing libmcrypt...........................
+if [ -f "/vshare/base-images/check_mk/server/libmcrypt-2.5.7-1.2.el6.rf.x86_64.rpm" ]; then
+	echo "libmcrypt-2.5.7-1.2.el6.rf.x86_64 found locally"
+	cp /vshare/base-images/check_mk/server/libmcrypt-2.5.7-1.2.el6.rf.x86_64.rpm .
+else
+	wget  http://pkgs.repoforge.org/libmcrypt/libmcrypt-2.5.7-1.2.el6.rf.x86_64.rpm
+fi
 yum -y install libmcrypt-2.5.7-1.2.el6.rf.x86_64.rpm
+
+echo ...........................Installing fping...........................
+if [ -f "/vshare/base-images/check_mk/server/fping-3.10-1.el6.rf.x86_64.rpm" ]; then
+	echo "fping-3.10-1.el6.rf.x86_64 found locally"
+	cp /vshare/base-images/check_mk/server/fping-3.10-1.el6.rf.x86_64.rpm .
+else
+	wget  http://pkgs.repoforge.org/fping/fping-3.10-1.el6.rf.x86_64.rpm
+fi
 yum -y install fping-3.10-1.el6.rf.x86_64.rpm
+
+echo ...........................Installing radiusclient...........................
+if [ -f "/vshare/base-images/check_mk/server/radiusclient-ng-0.5.6-5.el6.rf.x86_64.rpm" ]; then
+	echo "radiusclient-ng-0.5.6-5.el6.rf.x86_64 found locally"
+	cp /vshare/base-images/check_mk/server/radiusclient-ng-0.5.6-5.el6.rf.x86_64.rpm .
+else
+	wget  http://pkgs.repoforge.org/radiusclient-ng/radiusclient-ng-0.5.6-5.el6.rf.x86_64.rpm
+fi
 yum -y install radiusclient-ng-0.5.6-5.el6.rf.x86_64.rpm
+
+echo ...........................Installing OMD...........................
+if [ -f "/vshare/base-images/check_mk/server/omd-1.20.rhel7.x86_64.rpm" ]; then
+	echo "omd-1.20.rhel7.x86_64 found locally"
+	cp /vshare/base-images/check_mk/server/omd-1.20.rhel7.x86_64.rpm .
+else
+	wget http://files.omdistro.org/releases/centos_rhel/omd-1.20.rhel7.x86_64.rpm
+fi
 yum -y install omd-1.20.rhel7.x86_64.rpm
+
+echo ...........................Installing Check MK agent...........................
+if [ -f "/vshare/base-images/check_mk/agent/check_mk-agent-1.2.4p5-1.noarch.rpm" ]; then
+	echo "perl-Net-SNMP-5.2.0-1.2.el6.rf.noarch found locally"
+	cp /vshare/base-images/check_mk/agent/check_mk-agent-1.2.4p5-1.noarch.rpm .
+else
+	wget http://mathias-kettner.de/download/check_mk-agent-1.2.4p5-1.noarch.rpm
+fi
 yum -y install check_mk-agent-1.2.4p5-1.noarch.rpm
+
+echo ...........................Starting OMD server ..........................
 omd create monitoring
 omd start monitoring
+
+echo ...........................Allowing httpd processes to make outbound connections ..........................
 #SELinux on RHEL CentOS by default ships so that httpd processes cannot initiate outbound connections
 cd /usr/sbin
 setsebool -P httpd_can_network_connect 1
-#Install check mk agent
+
+echo ...........................Configuring check mk agent ..........................
 chmod 777 /etc/xinetd.d
-cp /mnt/gluster/repo/check_mk_forserver  /etc/xinetd.d/check_mk
+cp /mnt/gluster/repo/omd-setup/server/config/check_mk  /etc/xinetd.d/check_mk
 service xinetd restart
+
+echo ...........................Change configuration for SNMP..........................
 chmod 777 /etc/snmp/snmpd.conf
-cp -R /mnt/gluster/repo/snmpd.conf /etc/snmp/snmpd.conf
+cp -R /mnt/gluster/repo/omd-setup/server/config/snmpd.conf /etc/snmp/snmpd.conf
 service snmpd restart
 
-cp /mnt/gluster/repo/contacts.mk /omd/sites/monitoring/etc/check_mk/conf.d/wato/
-cp /mnt/gluster/repo/global.mk /omd/sites/monitoring/etc/check_mk/conf.d/wato/
-cp /mnt/gluster/repo/groups.mk /omd/sites/monitoring/etc/check_mk/conf.d/wato/
-cp /mnt/gluster/repo/rules.mk /omd/sites/monitoring/etc/check_mk/conf.d/wato/
-cp /mnt/gluster/repo/hosts.mk /omd/sites/monitoring/etc/check_mk/conf.d/wato/
+echo ...........................Change configuration for OMD server ..........................
+cp /mnt/gluster/repo/omd-setup/server/config/contacts.mk /omd/sites/monitoring/etc/check_mk/conf.d/wato/
+cp /mnt/gluster/repo/omd-setup/server/config/global.mk /omd/sites/monitoring/etc/check_mk/conf.d/wato/
+cp /mnt/gluster/repo/omd-setup/server/config/groups.mk /omd/sites/monitoring/etc/check_mk/conf.d/wato/
+cp /mnt/gluster/repo/omd-setup/server/config/rules.mk /omd/sites/monitoring/etc/check_mk/conf.d/wato/
+cp /mnt/gluster/repo/omd-setup/server/config/hosts.mk /omd/sites/monitoring/etc/check_mk/conf.d/wato/
 chown monitoring:monitoring /omd/sites/monitoring/etc/check_mk/conf.d/wato/*
-cp /mnt/gluster/repo/users.mk /omd/sites/monitoring/etc/check_mk/multisite.d/wato/
+
+cp /mnt/gluster/repo/omd-setup/server/config/users.mk /omd/sites/monitoring/etc/check_mk/multisite.d/wato/
 chown monitoring:monitoring /omd/sites/monitoring/etc/check_mk/multisite.d/wato/*
 
 # below code create hosts that are being monitored
@@ -51,9 +109,11 @@ chown monitoring:monitoring /omd/sites/monitoring/etc/check_mk/multisite.d/wato/
 #cp /mnt/gluster/repo/hosts.csv  /omd/versions/1.20/share/doc/check_mk/treasures
 #cp /mnt/gluster/repo/add_hosts.py  /omd/versions/1.20/share/doc/check_mk/treasures
 #yum -y install dos2unix
-su - monitoring
 #cd /omd/versions/1.20/share/doc/check_mk/treasures
 #dos2unix add_hosts.py
 #./add_hosts.py hosts.csv
+
+echo ...........................Restarting OMD server ..........................
+su - monitoring
 omd restart
 exit
