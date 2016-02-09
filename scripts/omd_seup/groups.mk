4cae971438632baf6ba36fbb88ddeba0158368b7
Krishna Kuntala <krishna.kuntala@mastek.com>
2016-02-09 14:18:39 +0000

    Restructured OMD agent and server installation directories. Added checks for local packages if available.

--- scripts/omd_seup/groups.mk
+++
@@ -1,7 +0,0 @@
-# Written by WATO
-# encoding: utf-8
-
-if type(define_contactgroups) != dict:
-    define_contactgroups = {}
-define_contactgroups.update({'DevOps': u'DevOps', 'all': u'Everybody'})
-
---
+++ scripts/omd_seup/server/config/users.mk
@@ -0,0 +1,14 @@
+# Written by Multisite UserDB
+# encoding: utf-8
+
+multisite_users = \
+{'devops-suite': {'alias': u'devops suite',
+                  'force_authuser': False,
+                  'force_authuser_webservice': False,
+                  'locked': True,
+                  'roles': ['user']},
+ 'omdadmin': {'alias': u'omdadmin',
+              'force_authuser': False,
+              'force_authuser_webservice': False,
+              'locked': False,
+              'roles': ['admin', 'user']}}
