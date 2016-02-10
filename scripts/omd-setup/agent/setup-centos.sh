f24ae30deb173e42201bd7691c1698c6e80ff34a
Krishna Kuntala <krishna.kuntala@mastek.com>
2016-02-10 11:42:22 +0000

    Modified docker and OMD scripts to point to same OMD agent configuration.
    Former-commit-id: 34b78ba8f88840bc3d6ce10e838e6b8ec48d2cc1

--- scripts/omd-setup/agent/setup-centos.sh
+++ scripts/omd-setup/agent/setup-centos.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+checkMKAgentRPMFilePath=$1
+checkMKAgentConfigFilePath=$2
+
 INSTALLATION_DIRECTORY=/opt/omd
 mkdir -p $INSTALLATION_DIRECTORY
 cd $INSTALLATION_DIRECTORY
 
 echo ...........................Installing check mk agent...........................
-if [ -f "/vshare/base-images/check_mk/agent/check_mk-agent-1.2.4p5-1.noarch.rpm" ]; then
-	yum install -y /vshare/base-images/check_mk/agent/check_mk-agent-1.2.4p5-1.noarch.rpm
+if [ -f $checkMKAgentRPMFilePath ]; then
+	yum install -y $checkMKAgentRPMFilePath
 else
 	wget http://mathias-kettner.de/download/check_mk-agent-1.2.4p5-1.noarch.rpm
 	yum -y install check_mk-agent-1.2.4p5-1.noarch.rpm
@@ -14,7 +17,7 @@ fi
 
 echo ...........................Configuring check mk agent...........................
 chmod 777 /etc/xinetd.d
-cp /mnt/gluster/repo/omd-setup/agent/config/check_mk  /etc/xinetd.d/check_mk
+cp $checkMKAgentConfigFilePath  /etc/xinetd.d/check_mk
 
 echo ...........................Restarting check mk agent...........................
 service xinetd restart
