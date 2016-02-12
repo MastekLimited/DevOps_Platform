cfde24a9c0f0afa06048b9a0ed0bd3928e7afd0b
Krishna Kuntala <krishna.kuntala@mastek.com>
2016-02-12 21:12:09 +0000

    Fixed ELK problem.
    Former-commit-id: e73a66af18f1dd9d0d9d7debfb4f50e8988baacf [formerly ae2ccdf7cbc8990b7dba8c74a747114532b6c709] [formerly 75e9d3acaeaefc11e8fc25c59d84a95de93985c5 [formerly efcc1c60a1e724559e5373e4a642e74c75444924]] [formerly 42051d6a6340a11d7bac89919c8e6d5ea6e0c3c6] [formerly a19a9a666f8058f91ff29047b84f36ccefba4f67 [formerly 040ece9a6798fad63b922d83536dbea78d4a9c11]] [formerly 0824d345a3d9eeac4f75cfd282277f588400c455] [formerly 69f010092e949d3f3ba8c9a18ea3b899ab1de995] [formerly e1c913c7e99ab285562eab7bd4bfce71a776d89d [formerly 85033e719ae6940a329ac594f00202d36df6423f] [formerly 69f010092e949d3f3ba8c9a18ea3b899ab1de995 [formerly 69f010092e949d3f3ba8c9a18ea3b899ab1de995 [formerly 69f010092e949d3f3ba8c9a18ea3b899ab1de995 [formerly 69f010092e949d3f3ba8c9a18ea3b899ab1de995 [formerly 69f010092e949d3f3ba8c9a18ea3b899ab1de995 [formerly 0a52a6304278690c0477566dec665e8098ac16a3]]]]]]]
    Former-commit-id: 968198b6b6adf3c858c0ce847dfaebb791d5ef60 [formerly 67e25f1c7bca4e99e36701f3266535a64c19dc8b] [formerly e0f3134317d03b65657080e666708f0c541f1f2c [formerly 84202c19eeddc90359752ca0ff9d980a8235dc01] [formerly e0f3134317d03b65657080e666708f0c541f1f2c [formerly 84202c19eeddc90359752ca0ff9d980a8235dc01] [formerly e0f3134317d03b65657080e666708f0c541f1f2c [formerly 84202c19eeddc90359752ca0ff9d980a8235dc01] [formerly e0f3134317d03b65657080e666708f0c541f1f2c [formerly 84202c19eeddc90359752ca0ff9d980a8235dc01] [formerly 84202c19eeddc90359752ca0ff9d980a8235dc01 [formerly ddb02815b289fca7d525372d431dbc5159c605b9]]]]]]
    Former-commit-id: e0f3134317d03b65657080e666708f0c541f1f2c [formerly e0f3134317d03b65657080e666708f0c541f1f2c [formerly 84202c19eeddc90359752ca0ff9d980a8235dc01]]
    Former-commit-id: 8f01f05f9b6ab140ea3b89c7ed6a2a8fc9169344
    Former-commit-id: 35d63a65c90c3ad0c74777200272f4f7901001e6 [formerly 3b4f7314d63aaab412403b78f196f3d0ca8bffce]
    Former-commit-id: 883c17a82633e5c7e87a8ce5862b59377029e158
    Former-commit-id: ec24c04fbba6dac274217d531a9fd28fc656a31e [formerly 50d909e7d2b5c5eb5a36580df09fe76d4f991c3b]
    Former-commit-id: f13f74e264cf86aaac0fbb702ca75f366197a003

--- docker/scripts/base/build.sh
+++ docker/scripts/base/build.sh
@@ -1,5 +1,6 @@
 #copy logstash-forwarder file to elk agent directory.
 cp /vshare/certificates/generated/logstash-forwarder.crt /mnt/gluster/repo/docker/roles/base/elk-setup/agent/config/
+cp /vshare/certificates/generated/logstash-forwarder.crt /vshare/docker/roles/base/elk-setup/agent/config/
 
 #build docker base image.
 docker build --no-cache -t base roles/base
