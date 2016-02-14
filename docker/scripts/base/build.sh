83c9fa01113c62fd7ef7a2a7b2121e2917ebae3a
Krishna Kuntala <krishna.kuntala@mastek.com>
2016-02-14 21:51:34 +0000

    Reverted local installables conditions.
    Former-commit-id: 70cca2d7480359744999d81add9410dc4cf17302 [formerly 08f8d3187719194005a9258997a2cfbc6f4d5a03] [formerly f5b4866d29e68b4244d7f65f5ecfc47126211e7a [formerly f5b4866d29e68b4244d7f65f5ecfc47126211e7a [formerly f5b4866d29e68b4244d7f65f5ecfc47126211e7a [formerly f5b4866d29e68b4244d7f65f5ecfc47126211e7a [formerly f5b4866d29e68b4244d7f65f5ecfc47126211e7a [formerly f5b4866d29e68b4244d7f65f5ecfc47126211e7a [formerly f5b4866d29e68b4244d7f65f5ecfc47126211e7a [formerly 38b3d5f14e33e91e444230c68cc75e52191e073c]]]]]]]]
    Former-commit-id: f5b4866d29e68b4244d7f65f5ecfc47126211e7a
    Former-commit-id: eebc6b8f049670e73c423484650839e76b9250a8
    Former-commit-id: df66b3b06a7cd453c08c527aa2922664dc425a45 [formerly 39c7ecf929cdcb3b43e7c9e558fd0765e81831fd] [formerly 346f59215a52172720e347249bd571bbda0ca016 [formerly 2bc681a4d9531ea4417e452e456807a20c886e68]]
    Former-commit-id: 6f1173c083a163f661db3cf008e6b6d190f748ab
    Former-commit-id: e1fa9bd1a52c913bfd08f587c01d38d3bb853296 [formerly 58c2c8054296005b444bbc670787104cd553edea] [formerly bf662e8c44b18662091e20db53dd6fe1558ecd45] [formerly 3a202031668fa2212dc5931c4606e275a1411e23 [formerly fdd4bbcf51628e45e84ec5cae16811c568474730] [formerly aea0d72cec466268f06f2f4050c26e7982814985 [formerly 9c3946cf31299916a45052790b3c945b5664979a]]]
    Former-commit-id: be61a38dfaebd6b59014e019f7a95d9478626b81
    Former-commit-id: 79f637fbe4dfaea2c79581d854266ecd82349b29
    Former-commit-id: 2c9b74f120199f0c6b979bf11898ae0232513233

--- docker/scripts/base/build.sh
+++ docker/scripts/base/build.sh
@@ -2,5 +2,9 @@
 cp /vshare/certificates/generated/logstash-forwarder.crt /mnt/gluster/repo/docker/roles/base/elk-setup/agent/config/
 cp /vshare/certificates/generated/logstash-forwarder.crt /vshare/docker/roles/base/elk-setup/agent/config/
 
+if [ -f "/vshare/base-images/docker/centos-6.6" ]; then
+	docker load -i /vshare/base-images/docker/centos-6.6
+fi
+
 #build docker base image.
 docker build --no-cache -t base roles/base
