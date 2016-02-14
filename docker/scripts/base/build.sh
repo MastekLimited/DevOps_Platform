5b902f0b91aa7fe8d1e09e5517e9f28c0747d921
Krishna Kuntala <krishna.kuntala@mastek.com>
2016-02-14 21:59:21 +0000

    Removed logstash-forwarder from vshare.
    Former-commit-id: a4f322756d68cc6737faf0015482f1ae5467fcff [formerly d800b6a2cb4d4f7363d4e8b6994f8c206960b4bd] [formerly ba838c72160aa8d0c9034db36f745c94435811b1] [formerly 3163013c53a37ad5841a1eaf07eb92110ba9274f [formerly 4c378d30e8318f858df200bbf8f7396cbca20d6a] [formerly 421913d8ce01e75b4fb0d60c25c459f0e74cbbb9 [formerly 39935aad98589879c656e1a7472a999dbc3197c1]]] [formerly 87f3f4a3326458f1d7bd58d993034f3dd47b09eb] [formerly 292243fcc2bcf1dba0c819981631870db7da8a29 [formerly 122963c62b345fd43c1ca85f43732858c891d59c]] [formerly c50013a664c71556fae7dfb681c2a7263810440e] [formerly 54d386258f5f7c72b5e3f9dd7ae0065a50eed03b] [formerly e17e375202aa3a9032fd1bf2930f1318f5056767 [formerly cca918567f367b712ca418a62023d9866efab778] [formerly 54d386258f5f7c72b5e3f9dd7ae0065a50eed03b [formerly 54d386258f5f7c72b5e3f9dd7ae0065a50eed03b [formerly 54d386258f5f7c72b5e3f9dd7ae0065a50eed03b [formerly 54d386258f5f7c72b5e3f9dd7ae0065a50eed03b [formerly 54d386258f5f7c72b5e3f9dd7ae0065a50eed03b [formerly 54d386258f5f7c72b5e3f9dd7ae0065a50eed03b [formerly eca33ee28611523804010395628d0571de9beab6]]]]]]]]
    Former-commit-id: b459c412063d95abc2db2bfb83571f23fe408886 [formerly 3c9e4211bd36f6699e51f2619e8fd8bf5043e715] [formerly 1ae51dc074ec7699147f246985d30221ef2473ed [formerly 5acdd36def627ac466dfda0591a4d772efb807f4] [formerly 5acdd36def627ac466dfda0591a4d772efb807f4 [formerly 5acdd36def627ac466dfda0591a4d772efb807f4 [formerly 5acdd36def627ac466dfda0591a4d772efb807f4 [formerly 5acdd36def627ac466dfda0591a4d772efb807f4 [formerly 5acdd36def627ac466dfda0591a4d772efb807f4 [formerly 60fa934df69ad45386684ea5558a78cff9ad0479]]]]]]]
    Former-commit-id: 1ae51dc074ec7699147f246985d30221ef2473ed [formerly 5acdd36def627ac466dfda0591a4d772efb807f4]
    Former-commit-id: 0b17eb840e09ef1f32eb8a3f2a15cac756734e04
    Former-commit-id: 866e346526287c27d59c01def9738e96bcebb7d9 [formerly e26ad18a3befaca97735570df30c4477f310b96a]
    Former-commit-id: 6f68bdd8e46007ba0377b247f9fbcd36d63e18b6
    Former-commit-id: 1d031815a12095fe0505ab40c551fe1201dbf109 [formerly 351113c9ef3054f92d5527e1660a26969483923d] [formerly ae2396b10622a987bd9095670dd6479d57ceb3c3 [formerly 2d1feef566727b9c189ac599487eaefd745857d9]]
    Former-commit-id: 1246a7b29117bfff4204290732f78f9b9132bc71
    Former-commit-id: 49202c9a68e467bdd9d06d0a4060d51fee729a30

--- docker/scripts/base/build.sh
+++ docker/scripts/base/build.sh
@@ -1,6 +1,5 @@
 #copy logstash-forwarder file to elk agent directory.
-cp /vshare/certificates/generated/logstash-forwarder.crt /mnt/gluster/repo/docker/roles/base/elk-setup/agent/config/
-cp /vshare/certificates/generated/logstash-forwarder.crt /vshare/docker/roles/base/elk-setup/agent/config/
+cp /mnt/gluster/repo/certificates/generated/logstash-forwarder.crt /mnt/gluster/repo/docker/roles/base/elk-setup/agent/config/
 
 if [ -f "/vshare/base-images/docker/centos-6.6" ]; then
 	docker load -i /vshare/base-images/docker/centos-6.6
