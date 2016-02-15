1b3ea39c94c62fd4bb44387935798b28b5058764
Krishna Kuntala <krishna.kuntala@mastek.com>
2016-02-15 16:47:07 +0000

    Setting up Docker using local installables.
    Former-commit-id: 943f78cdba795a75982516aec791a66dde2cae0c [formerly cab1344e1adbf670c7ed56023699aac884a24f3d] [formerly 54d30a3c8bb087b9f2ddf8cf9deae9b6aab0040a [formerly 54d30a3c8bb087b9f2ddf8cf9deae9b6aab0040a [formerly 54d30a3c8bb087b9f2ddf8cf9deae9b6aab0040a [formerly 54d30a3c8bb087b9f2ddf8cf9deae9b6aab0040a [formerly 54d30a3c8bb087b9f2ddf8cf9deae9b6aab0040a [formerly 54d30a3c8bb087b9f2ddf8cf9deae9b6aab0040a [formerly 08c9548b3bc16eb19d34e8f69cd90f689eca990e]]]]]]]
    Former-commit-id: 54d30a3c8bb087b9f2ddf8cf9deae9b6aab0040a
    Former-commit-id: b407da219f55b62a958c3d972087f7889501c523
    Former-commit-id: 4b9969d790d59b37840268fec3875ff1813571e9 [formerly 4a6aed8dd94074b18bcd519d356affeab2deca95] [formerly 4ffbaa4d97ae31133c6762561dba1f0c75c5ccaa [formerly ff0e5394a115a4f26132eb81b79995659103f77f]]
    Former-commit-id: 139f69b47715053751327beeede992fa70db211f
    Former-commit-id: 76c49de716db04bf0b1269260ab54762f88e36f2 [formerly 9906252fa0aba9a21dbec0c8935448eda221e889] [formerly 2d0922a6e51a65f2a9b77737668933eb31bb7bc3 [formerly a2785e9f443b280073e294655e14194014fa3a19]]
    Former-commit-id: 5e43febc50799940f281673eab5e03643232cf29
    Former-commit-id: 921fd3edd9612e368cbc3e957780256e2b31e322

--- scripts/elk-setup/agent/setup-centos.sh
+++ scripts/elk-setup/agent/setup-centos.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
-logstashForwarderConfigFilePath=$1
-logstashForwarderCertificateFilePath=$2
-logstashForwarderRepoFilePath=$3
+logstashForwarderConfigDirectoryPath=$1
 
+logstashForwarderConfigFilePath=$logstashForwarderConfigDirectoryPath/logstash-forwarder.conf
+logstashForwarderCertificateFilePath=$logstashForwarderConfigDirectoryPath/logstash-forwarder.crt
+logstashForwarderRepoFilePath=$logstashForwarderConfigDirectoryPath/logstash-forwarder.repo
+
+logstashForwarderInstallablesDirectoryPath=$2
+
+gpgKeyElasticsearchFilePath=$logstashForwarderInstallablesDirectoryPath/GPG-KEY-elasticsearch
+logstashForwarderRPMFilePath=$logstashForwarderInstallablesDirectoryPath/check_mk-agent-1.2.4p5-1.noarch.rpm
+
+echo ...........................Installing logstash-forwarder...........................
 rpm --import http://packages.elasticsearch.org/GPG-KEY-elasticsearch
 cp $logstashForwarderRepoFilePath  /etc/yum.repos.d
 yum -y install logstash-forwarder
+
 cp $logstashForwarderCertificateFilePath /etc/pki/tls/certs
-#OR
-#scp root@&&ELK_HOST_IP&&:/mnt/gluster/repo/logstash-forwarder.crt /etc/pki/tls/certs/
 cp $logstashForwarderConfigFilePath  /etc
 service logstash-forwarder restart
 exit
