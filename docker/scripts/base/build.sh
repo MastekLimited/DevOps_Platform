2c1e52a7a364924b914eb9aa5f84643efecf6b17
Krishna Kuntala <krishna.kuntala@mastek.com>
2016-02-15 16:47:07 +0000

    Setting up Docker using local installables.
    Former-commit-id: 943f78cdba795a75982516aec791a66dde2cae0c [formerly cab1344e1adbf670c7ed56023699aac884a24f3d] [formerly 54d30a3c8bb087b9f2ddf8cf9deae9b6aab0040a [formerly 54d30a3c8bb087b9f2ddf8cf9deae9b6aab0040a [formerly 54d30a3c8bb087b9f2ddf8cf9deae9b6aab0040a [formerly 54d30a3c8bb087b9f2ddf8cf9deae9b6aab0040a [formerly 54d30a3c8bb087b9f2ddf8cf9deae9b6aab0040a [formerly 54d30a3c8bb087b9f2ddf8cf9deae9b6aab0040a [formerly 08c9548b3bc16eb19d34e8f69cd90f689eca990e]]]]]]]
    Former-commit-id: 54d30a3c8bb087b9f2ddf8cf9deae9b6aab0040a
    Former-commit-id: b407da219f55b62a958c3d972087f7889501c523
    Former-commit-id: 4a6aed8dd94074b18bcd519d356affeab2deca95 [formerly ff0e5394a115a4f26132eb81b79995659103f77f]
    Former-commit-id: 139f69b47715053751327beeede992fa70db211f
    Former-commit-id: b51c86d71da6c7a32ca6f67a8ee57cb224b833a8 [formerly 2d0922a6e51a65f2a9b77737668933eb31bb7bc3] [formerly 9268f8ad8828370735a24d1539b68fcf4816815f [formerly a2785e9f443b280073e294655e14194014fa3a19]]
    Former-commit-id: 9906252fa0aba9a21dbec0c8935448eda221e889
    Former-commit-id: 76c49de716db04bf0b1269260ab54762f88e36f2

--- docker/scripts/base/build.sh
+++ docker/scripts/base/build.sh
@@ -5,5 +5,19 @@ if [ -f "/vshare/base-images/docker/centos-6.6" ]; then
 	docker load -i /vshare/base-images/docker/centos-6.6
 fi
 
+echo ...........................Copying installables to docker.roles.base.installables...........................
+
+mkdir /mnt/gluster/repo/docker/roles/base/installables/
+cp /vshare/base-images/misc /mnt/gluster/repo/docker/roles/base/installables/
+cp /vshare/base-images/jdk /mnt/gluster/repo/docker/roles/base/installables/
+
+mkdir /mnt/gluster/repo/docker/roles/base/elk-setup/agent/installables/
+cp /vshare/base-images/elk/GPG-KEY-elasticsearch /mnt/gluster/repo/docker/roles/base/elk-setup/agent/installables/
+cp /vshare/base-images/elk/logstash-forwarder-0.4.0-1.x86_64.rpm /mnt/gluster/repo/docker/roles/base/elk-setup/agent/installables/
+
+mkdir /mnt/gluster/repo/docker/roles/base/omd-setup/agent/installables/
+cp /vshare/base-images/elk/GPG-KEY-elasticsearch /mnt/gluster/repo/docker/roles/base/omd-setup/agent/installables/
+cp /vshare/base-images/elk/logstash-forwarder-0.4.0-1.x86_64.rpm /mnt/gluster/repo/docker/roles/base/omd-setup/agent/installables/
+
 #build docker base image.
 docker build --no-cache -t base roles/base
