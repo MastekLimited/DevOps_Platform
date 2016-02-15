44fdb3a9e2fd6330ddcb417a3fc7ed76731b5708
Krishna Kuntala <krishna.kuntala@mastek.com>
2016-02-15 23:04:06 +0000

    Fixed errors.

--- docker/scripts/base/build.sh
+++ docker/scripts/base/build.sh
@@ -8,16 +8,15 @@ fi
 echo ...........................Copying installables to docker.roles.base.installables...........................
 
 mkdir /mnt/gluster/repo/docker/roles/base/installables/
-cp /vshare/base-images/misc /mnt/gluster/repo/docker/roles/base/installables/
-cp /vshare/base-images/jdk /mnt/gluster/repo/docker/roles/base/installables/
+cp -R /vshare/base-images/misc /mnt/gluster/repo/docker/roles/base/installables/
+cp -R /vshare/base-images/jdk /mnt/gluster/repo/docker/roles/base/installables/
 
 mkdir /mnt/gluster/repo/docker/roles/base/elk-setup/agent/installables/
 cp /vshare/base-images/elk/GPG-KEY-elasticsearch /mnt/gluster/repo/docker/roles/base/elk-setup/agent/installables/
 cp /vshare/base-images/elk/logstash-forwarder-0.4.0-1.x86_64.rpm /mnt/gluster/repo/docker/roles/base/elk-setup/agent/installables/
 
 mkdir /mnt/gluster/repo/docker/roles/base/omd-setup/agent/installables/
-cp /vshare/base-images/elk/GPG-KEY-elasticsearch /mnt/gluster/repo/docker/roles/base/omd-setup/agent/installables/
-cp /vshare/base-images/elk/logstash-forwarder-0.4.0-1.x86_64.rpm /mnt/gluster/repo/docker/roles/base/omd-setup/agent/installables/
+cp /vshare/base-images/check_mk/agent/check_mk-agent-1.2.4p5-1.noarch.rpm /mnt/gluster/repo/docker/roles/base/omd-setup/agent/installables/
 
 #build docker base image.
 docker build --no-cache -t base roles/base
