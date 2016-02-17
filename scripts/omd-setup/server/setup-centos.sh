c586157fc3b8ad5846a51c292a84b25aa70fb423
Krishna Kuntala <krishna.kuntala@mastek.com>
2016-02-17 11:30:53 +0000

    Most of the installations locally, except OMD.
    Former-commit-id: 8c0e6fe5f06cce81588c9c95620cb170033c9f72 [formerly ed2593afb78e68d0712a5e34eb12cdc1dfd698c6] [formerly a09a26a02a7c79ea9b66c907f1617be77225554c] [formerly a79a49ac49cd840d74c663bf034068125787849c [formerly 206828a0c5b5b6deee78b3eba3ded3b0db08c621] [formerly 122e88120d3fafd79d557c18b143efa7f3b79de2 [formerly 66ec36c6b92c013450f155af523345a8a0cd6e2d]]] [formerly 1d327a1cedf916d5811b77a6a82f9a315561814b] [formerly 5ecf9f99d3a3d98eb7bc86a8fd7a9403523054c5 [formerly ab7357d882df9e3bfd5c0b3d00c7a3ec0a83336a] [formerly bc347908bba557e08ea85cef8793e56e5d9530f2 [formerly 0cfba90f199c30413ab5cffc753b2e86c988003b]]] [formerly ab9d907f730699d7a8864d163648fc4cefc6c46d] [formerly 23180e393bbdeb372c23277f7812b26a0fd125ce] [formerly 7fcd038563d6debcb6de65cdd1dd61fdde18deeb [formerly 8cba234cba4696234519b2b46a86a68b6ee722a8] [formerly 23180e393bbdeb372c23277f7812b26a0fd125ce [formerly 23180e393bbdeb372c23277f7812b26a0fd125ce [formerly 23180e393bbdeb372c23277f7812b26a0fd125ce [formerly 23180e393bbdeb372c23277f7812b26a0fd125ce [formerly 23180e393bbdeb372c23277f7812b26a0fd125ce [formerly 23180e393bbdeb372c23277f7812b26a0fd125ce [formerly 2e0085fd55e22c2173a2a8ed31311266398044be]]]]]]]]
    Former-commit-id: fe66b4432c40df79434ab3e4271322abe617e958 [formerly 2361a40b0b8eac34bc2d960ad6195eef2c9e41f9] [formerly c3669898abd27787e6ed5921c8fef881a05429c7 [formerly c3669898abd27787e6ed5921c8fef881a05429c7 [formerly c3669898abd27787e6ed5921c8fef881a05429c7 [formerly c3669898abd27787e6ed5921c8fef881a05429c7 [formerly c3669898abd27787e6ed5921c8fef881a05429c7 [formerly c3669898abd27787e6ed5921c8fef881a05429c7 [formerly 7cfb67a4aed62052d154ba953cc8be2adf609452]]]]]]]
    Former-commit-id: c3669898abd27787e6ed5921c8fef881a05429c7
    Former-commit-id: 82bbd89cd46dfc1ed6e12204fc04e75ee8bdf98e
    Former-commit-id: a251e8a8803eae4c0a8e53810fd3d25ee1dbff54 [formerly 5793ac7f9e37ae481757c238fb8cf233a1864346] [formerly aa338db6067aac8dce7db70478c1bb448069ac1b [formerly 9421e2d31875db983be61d68b82e5afc181a460f]]
    Former-commit-id: db79893e874afeb9ae795ff4bacbfcfd3b1ad7e3
    Former-commit-id: 620803b7a8aa1a729eea98bcc33af89839a6482b [formerly 8e0c5f2b0e5e03aa2f00346862f8c2d0eb57173b] [formerly 1b6783045934b1d19c46bc8abecf7014874cb3f5 [formerly 545f72937b44d9b90ecd82f3bad692c8e9c20f6b]]
    Former-commit-id: 3ae2a85d90d67dc76094f9858cbab9b621edaf50
    Former-commit-id: 7e37dd4ca5007188bfaec913496eccaca97432b1

--- scripts/omd-setup/server/setup-centos.sh
+++ scripts/omd-setup/server/setup-centos.sh
@@ -5,19 +5,91 @@ chmod 777 *
 
 yum remove MySQL-*
 
-echo ...........................Installing mariadb...........................
-if [ -f "/vshare/base-images/check_mk/server/mariadb-server-5.5.44-2.el7.centos.x86_64.rpm" ]; then
-	yum install -y /vshare/base-images/check_mk/server/mariadb-server-5.5.44-2.el7.centos.x86_64.rpm mariadb
+echo '================================================================================'
+echo $'			Installing mariadb: START'
+echo '================================================================================'
+
+if [ -f "/vshare/base-images/check_mk/server/mariadb-libs-5.5.44-2.el7.centos.x86_64.rpm" ]; then
+	yum install -y --skip-broken /vshare/base-images/check_mk/server/mariadb-libs-5.5.44-2.el7.centos.x86_64.rpm
+fi
+
+if [ -f "/vshare/base-images/check_mk/server/perl-Data-Dumper-2.145-3.el7.x86_64.rpm" ]; then
+	yum install -y --skip-broken /vshare/base-images/check_mk/server/perl-Data-Dumper-2.145-3.el7.x86_64.rpm
+fi
+
+if [ -f "/vshare/base-images/check_mk/server/perl-Compress-Raw-Bzip2-2.061-3.el7.x86_64.rpm" ]; then
+	yum install -y --skip-broken /vshare/base-images/check_mk/server/perl-Compress-Raw-Bzip2-2.061-3.el7.x86_64.rpm
+fi
+
+if [ -f "/vshare/base-images/check_mk/server/perl-Compress-Raw-Zlib-2.061-4.el7.x86_64.rpm" ]; then
+	yum install -y --skip-broken /vshare/base-images/check_mk/server/perl-Compress-Raw-Zlib-2.061-4.el7.x86_64.rpm
+fi
+
+if [ -f "/vshare/base-images/check_mk/server/perl-IO-Compress-2.061-2.el7.noarch.rpm" ]; then
+	yum install -y --skip-broken /vshare/base-images/check_mk/server/perl-IO-Compress-2.061-2.el7.noarch.rpm
+fi
+
+if [ -f "/vshare/base-images/check_mk/server/perl-Net-Daemon-0.48-5.el7.noarch.rpm" ]; then
+	yum install -y --skip-broken /vshare/base-images/check_mk/server/perl-Net-Daemon-0.48-5.el7.noarch.rpm
+fi
+
+if [ -f "/vshare/base-images/check_mk/server/perl-PlRPC-0.2020-14.el7.noarch.rpm" ]; then
+	yum install -y --skip-broken /vshare/base-images/check_mk/server/perl-PlRPC-0.2020-14.el7.noarch.rpm
+fi
+
+if [ -f "/vshare/base-images/check_mk/server/perl-DBI-1.627-4.el7.x86_64.rpm" ]; then
+	yum install -y --skip-broken /vshare/base-images/check_mk/server/perl-DBI-1.627-4.el7.x86_64.rpm
+fi
+
+if [ -f "/vshare/base-images/check_mk/server/mariadb-server-5.5.44-2.el7.centos.x86_64.rpm" ] && [ -f "/vshare/base-images/check_mk/server/mariadb-5.5.44-2.el7.centos.x86_64.rpm" ]; then
+	yum install -y /vshare/base-images/check_mk/server/mariadb-server-5.5.44-2.el7.centos.x86_64.rpm /vshare/base-images/check_mk/server/mariadb-5.5.44-2.el7.centos.x86_64.rpm
 else
 	yum -y install mariadb-server mariadb
 fi
 
-echo ...........................Starting mariadb...........................
+echo '================================================================================'
+echo $'			Installing mariadb: END'
+echo '================================================================================'
+
+echo '================================================================================'
+echo $'			Starting mariadb'
+echo '================================================================================'
 systemctl start mariadb
 systemctl enable mariadb
 systemctl start mariadb
 
-echo ...........................Installing SNMP...........................
+echo '================================================================================'
+echo $'			Installing SNMP: START'
+echo '================================================================================'
+
+if [ -f "/vshare/base-images/check_mk/server/perl-Digest-1.17-245.el7.noarch.rpm" ]; then
+	yum install -y --skip-broken /vshare/base-images/check_mk/server/perl-Digest-1.17-245.el7.noarch.rpm
+fi
+
+if [ -f "/vshare/base-images/check_mk/server/perl-Digest-MD5-2.52-3.el7.x86_64.rpm" ]; then
+	yum install -y --skip-broken /vshare/base-images/check_mk/server/perl-Digest-MD5-2.52-3.el7.x86_64.rpm
+fi
+
+if [ -f "/vshare/base-images/check_mk/server/perl-Digest-SHA-5.85-3.el7.x86_64.rpm" ]; then
+	yum install -y --skip-broken /vshare/base-images/check_mk/server/perl-Digest-SHA-5.85-3.el7.x86_64.rpm
+fi
+
+if [ -f "/vshare/base-images/check_mk/server/perl-Digest-HMAC-1.03-5.el7.noarch.rpm" ]; then
+	yum install -y --skip-broken /vshare/base-images/check_mk/server/perl-Digest-HMAC-1.03-5.el7.noarch.rpm
+fi
+
+if [ -f "/vshare/base-images/check_mk/server/perl-Crypt-DES-2.05-20.el7.x86_64.rpm" ]; then
+	yum install -y --skip-broken /vshare/base-images/check_mk/server/perl-Crypt-DES-2.05-20.el7.x86_64.rpm
+fi
+
+if [ -f "/vshare/base-images/check_mk/server/perl-Digest-SHA1-2.13-9.el7.x86_64.rpm" ]; then
+	yum install -y --skip-broken /vshare/base-images/check_mk/server/perl-Digest-SHA1-2.13-9.el7.x86_64.rpm
+fi
+
+if [ -f "/vshare/base-images/check_mk/server/perl-Socket6-0.23-15.el7.x86_64.rpm" ]; then
+	yum install -y --skip-broken /vshare/base-images/check_mk/server/perl-Socket6-0.23-15.el7.x86_64.rpm
+fi
+
 if [ -f "/vshare/base-images/check_mk/server/perl-Net-SNMP-5.2.0-1.2.el6.rf.noarch.rpm" ]; then
 	cp /vshare/base-images/check_mk/server/perl-Net-SNMP-5.2.0-1.2.el6.rf.noarch.rpm .
 else
@@ -25,7 +97,13 @@ else
 fi
 yum -y install perl-Net-SNMP-5.2.0-1.2.el6.rf.noarch.rpm
 
-echo ...........................Installing libmcrypt...........................
+echo '================================================================================'
+echo $'			Installing SNMP: END'
+echo '================================================================================'
+
+echo '================================================================================'
+echo $'			Installing libmcrypt'
+echo '================================================================================'
 if [ -f "/vshare/base-images/check_mk/server/libmcrypt-2.5.7-1.2.el6.rf.x86_64.rpm" ]; then
 	cp /vshare/base-images/check_mk/server/libmcrypt-2.5.7-1.2.el6.rf.x86_64.rpm .
 else
@@ -33,7 +111,9 @@ else
 fi
 yum -y install libmcrypt-2.5.7-1.2.el6.rf.x86_64.rpm
 
-echo ...........................Installing fping...........................
+echo '================================================================================'
+echo $'			Installing fping'
+echo '================================================================================'
 if [ -f "/vshare/base-images/check_mk/server/fping-3.10-1.el6.rf.x86_64.rpm" ]; then
 	cp /vshare/base-images/check_mk/server/fping-3.10-1.el6.rf.x86_64.rpm .
 else
@@ -41,7 +121,9 @@ else
 fi
 yum -y install fping-3.10-1.el6.rf.x86_64.rpm
 
-echo ...........................Installing radiusclient...........................
+echo '================================================================================'
+echo $'			Installing radiusclient'
+echo '================================================================================'
 if [ -f "/vshare/base-images/check_mk/server/radiusclient-ng-0.5.6-5.el6.rf.x86_64.rpm" ]; then
 	cp /vshare/base-images/check_mk/server/radiusclient-ng-0.5.6-5.el6.rf.x86_64.rpm .
 else
@@ -49,7 +131,9 @@ else
 fi
 yum -y install radiusclient-ng-0.5.6-5.el6.rf.x86_64.rpm
 
-echo ...........................Installing OMD...........................
+echo '================================================================================'
+echo $'			Installing OMD'
+echo '================================================================================'
 if [ -f "/vshare/base-images/check_mk/server/omd-1.20.rhel7.x86_64.rpm" ]; then
 	cp /vshare/base-images/check_mk/server/omd-1.20.rhel7.x86_64.rpm .
 else
@@ -57,7 +141,9 @@ else
 fi
 yum -y install omd-1.20.rhel7.x86_64.rpm
 
-echo ...........................Installing Check MK agent...........................
+echo '================================================================================'
+echo $'			Installing Check MK agent'
+echo '================================================================================'
 if [ -f "/vshare/base-images/check_mk/agent/check_mk-agent-1.2.4p5-1.noarch.rpm" ]; then
 	cp /vshare/base-images/check_mk/agent/check_mk-agent-1.2.4p5-1.noarch.rpm .
 else
@@ -65,26 +151,36 @@ else
 fi
 yum -y install check_mk-agent-1.2.4p5-1.noarch.rpm
 
-echo ...........................Starting OMD server ..........................
+echo '================================================================================'
+echo $'			Starting OMD server'
+echo '================================================================================'
 omd create monitoring
 omd start monitoring
 
-echo ...........................Allowing httpd processes to make outbound connections ..........................
+echo '================================================================================'
+echo $'		Allowing httpd processes to make outbound connection'
+echo '================================================================================'
 #SELinux on RHEL CentOS by default ships so that httpd processes cannot initiate outbound connections
 cd /usr/sbin
 setsebool -P httpd_can_network_connect 1
 
-echo ...........................Configuring check mk agent ..........................
+echo '================================================================================'
+echo $'			Configuring check mk agent'
+echo '================================================================================'
 chmod 777 /etc/xinetd.d
 cp /mnt/gluster/repo/omd-setup/server/config/check_mk  /etc/xinetd.d/check_mk
 service xinetd restart
 
-echo ...........................Change configuration for SNMP..........................
+echo '================================================================================'
+echo $'			Change configuration for SNMP'
+echo '================================================================================'
 chmod 777 /etc/snmp/snmpd.conf
 cp -R /mnt/gluster/repo/omd-setup/server/config/snmpd.conf /etc/snmp/snmpd.conf
 service snmpd restart
 
-echo ...........................Change configuration for OMD server ..........................
+echo '================================================================================'
+echo $'			Change configuration for OMD server'
+echo '================================================================================'
 cp /mnt/gluster/repo/omd-setup/server/config/contacts.mk /omd/sites/monitoring/etc/check_mk/conf.d/wato/
 cp /mnt/gluster/repo/omd-setup/server/config/global.mk /omd/sites/monitoring/etc/check_mk/conf.d/wato/
 cp /mnt/gluster/repo/omd-setup/server/config/groups.mk /omd/sites/monitoring/etc/check_mk/conf.d/wato/
@@ -107,7 +203,9 @@ chown monitoring:monitoring /omd/sites/monitoring/etc/check_mk/multisite.d/wato/
 #dos2unix add_hosts.py
 #./add_hosts.py hosts.csv
 
-echo ...........................Restarting OMD server ..........................
+echo '================================================================================'
+echo $'			Restarting OMD server'
+echo '================================================================================'
 su - monitoring
 omd restart
 exit
