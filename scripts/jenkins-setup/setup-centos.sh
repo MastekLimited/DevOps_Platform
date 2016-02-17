045c770aa35271df3da95630bf63ee2315a98a73
Krishna Kuntala <krishna.kuntala@mastek.com>
2016-02-17 16:00:06 +0000

    Changed repo path
    Former-commit-id: ae6ba8e85eef7e0d525e14ff7b2873837d0d88b5 [formerly 6ea45a9e596c35e3a0644448fb9172c4d0354439] [formerly ba5b4a89540561c17c0b664f63a9a9290bbd69af [formerly ba5b4a89540561c17c0b664f63a9a9290bbd69af [formerly ba5b4a89540561c17c0b664f63a9a9290bbd69af [formerly ba5b4a89540561c17c0b664f63a9a9290bbd69af [formerly ba5b4a89540561c17c0b664f63a9a9290bbd69af [formerly ba5b4a89540561c17c0b664f63a9a9290bbd69af [formerly 96b75e815da863372caf7e871d22cc0c450a291f]]]]]]]
    Former-commit-id: ba5b4a89540561c17c0b664f63a9a9290bbd69af
    Former-commit-id: 4c7be70acbda3dbdc05d9358554affb231922fad
    Former-commit-id: dbb2ad8531e9272e7ef1c19ef8b50133e5d41de7 [formerly 97ce5179679bb6f409be99513f05e7858a21452d]
    Former-commit-id: ae8bb2ac194cd239b07a5638fdff667ea68af8b1
    Former-commit-id: a1690d1669efd0d0d5ba896008cac0c86d4d768a [formerly 1b4488d63cc07c1e1bb9eb6f38e12dc5fe8eca78] [formerly 5fb4a543303a78ff99f0fe449e2d9290e81b2906 [formerly 83c3f952cd083cfbc036d2521d55b8a29be04bf6]]
    Former-commit-id: e855e4048762b86923d805b9e4b5a610ae7a94b7
    Former-commit-id: e2d983e69f0e7b5ea9c9fbb5410df2c47c0e62cf

--- scripts/jenkins-setup/setup-centos.sh
+++ scripts/jenkins-setup/setup-centos.sh
@@ -5,8 +5,8 @@ cd /opt;
 echo '================================================================================'
 echo '			Installing java'
 echo '================================================================================'
-if [ -f "/vshare/base-images/jdk/jdk-8u45-linux-x64.rpm" ]; then
-	yum install -y /vshare/base-images/jdk/jdk-8u45-linux-x64.rpm
+if [ -f "/vshare/repo/jdk/jdk-8u45-linux-x64.rpm" ]; then
+	yum install -y /vshare/repo/jdk/jdk-8u45-linux-x64.rpm
 else
 	wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u45-b14/jdk-8u45-linux-x64.rpm";
 	rpm -Uvh jdk-8u45-linux-x64.rpm;
@@ -15,8 +15,8 @@ fi
 echo '================================================================================'
 echo '			Installing sshpass'
 echo '================================================================================'
-if [ -f "/vshare/base-images/misc/sshpass-1.05-9.2.x86_64.rpm" ]; then
-	yum --enablerepo=epel -y install /vshare/base-images/misc/sshpass-1.05-9.2.x86_64.rpm
+if [ -f "/vshare/repo/misc/sshpass-1.05-9.2.x86_64.rpm" ]; then
+	yum --enablerepo=epel -y install /vshare/repo/misc/sshpass-1.05-9.2.x86_64.rpm
 else
 	yum --enablerepo=epel -y install sshpass
 fi
@@ -25,20 +25,20 @@ echo '==========================================================================
 echo '			Installing git: START'
 echo '================================================================================'
 
-if [ -f "/vshare/base-images/git/perl-Error-0.17020-2.el7.noarch.rpm" ]; then
-	yum install -y --skip-broken /vshare/base-images/git/perl-Error-0.17020-2.el7.noarch.rpm
+if [ -f "/vshare/repo/git/perl-Error-0.17020-2.el7.noarch.rpm" ]; then
+	yum install -y --skip-broken /vshare/repo/git/perl-Error-0.17020-2.el7.noarch.rpm
 fi
 
-if [ -f "/vshare/base-images/git/libgnome-keyring-3.8.0-3.el7.x86_64.rpm" ]; then
-	yum install -y --skip-broken /vshare/base-images/git/libgnome-keyring-3.8.0-3.el7.x86_64.rpm
+if [ -f "/vshare/repo/git/libgnome-keyring-3.8.0-3.el7.x86_64.rpm" ]; then
+	yum install -y --skip-broken /vshare/repo/git/libgnome-keyring-3.8.0-3.el7.x86_64.rpm
 fi
 
-if [ -f "/vshare/base-images/git/perl-TermReadKey-2.30-20.el7.x86_64.rpm" ]; then
-	yum install -y --skip-broken /vshare/base-images/git/perl-TermReadKey-2.30-20.el7.x86_64.rpm
+if [ -f "/vshare/repo/git/perl-TermReadKey-2.30-20.el7.x86_64.rpm" ]; then
+	yum install -y --skip-broken /vshare/repo/git/perl-TermReadKey-2.30-20.el7.x86_64.rpm
 fi
 
-if [ -f "/vshare/base-images/git/git-1.8.3.1-6.el7.x86_64.rpm" ] && [ -f "/vshare/base-images/git/perl-Git-1.8.3.1-6.el7.noarch.rpm" ]; then
-	yum install -y /vshare/base-images/git/git-1.8.3.1-6.el7.x86_64.rpm /vshare/base-images/git/perl-Git-1.8.3.1-6.el7.noarch.rpm
+if [ -f "/vshare/repo/git/git-1.8.3.1-6.el7.x86_64.rpm" ] && [ -f "/vshare/repo/git/perl-Git-1.8.3.1-6.el7.noarch.rpm" ]; then
+	yum install -y /vshare/repo/git/git-1.8.3.1-6.el7.x86_64.rpm /vshare/repo/git/perl-Git-1.8.3.1-6.el7.noarch.rpm
 else
 	yum install -y git
 fi
@@ -50,15 +50,15 @@ echo '==========================================================================
 echo '================================================================================'
 echo '			Installing jenkins'
 echo '================================================================================'
-if [ -f "/vshare/base-images/jenkins/jenkins-ci.org.key" ]; then
-	rpm --import /vshare/base-images/jenkins/jenkins-ci.org.key
+if [ -f "/vshare/repo/jenkins/jenkins-ci.org.key" ]; then
+	rpm --import /vshare/repo/jenkins/jenkins-ci.org.key
 else
 	wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
 	rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
 fi
 
-if [ -f "/vshare/base-images/jenkins/jenkins-1.642.1-1.1.noarch.rpm" ]; then
-	yum -y install /vshare/base-images/jenkins/jenkins-1.642.1-1.1.noarch.rpm
+if [ -f "/vshare/repo/jenkins/jenkins-1.642.1-1.1.noarch.rpm" ]; then
+	yum -y install /vshare/repo/jenkins/jenkins-1.642.1-1.1.noarch.rpm
 else
 	yum -y install jenkins
 fi
