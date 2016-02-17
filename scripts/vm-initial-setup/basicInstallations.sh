962a37cb9cba11bf6306c7204e626ac8d3a1438d
Krishna Kuntala <krishna.kuntala@mastek.com>
2016-02-17 12:02:28 +0000

    Changed echo statments.
    Former-commit-id: 8943370d3482020462a1b4246d76669277c9bc1c [formerly 8827b1e7848787f6e8013ff4d27cd9b5a097d484] [formerly 34e32ceb7b27d14cf6007dbc24284eaf82782f7b [formerly 1660355c697863fdceb3a22936641bf03e59cb68]] [formerly ca6d7c2ebcb68a4cdc24448c8801df997ce92a9e] [formerly 35aa28423445c11f4cd7d3070d3fbd3a73d44cef [formerly 2fb48c72c6f82f461f16b4967218143486e21070]] [formerly dbca29595fd79534050b2010b4265af8934fba05] [formerly 898444608438bc92f66973ffb06b532b8b713448] [formerly f2e0110b70609b6bd5f380868361fbb585146bbf [formerly 721097e2819bccd5e54e5c171156cdaedda5e231] [formerly 898444608438bc92f66973ffb06b532b8b713448 [formerly 898444608438bc92f66973ffb06b532b8b713448 [formerly 898444608438bc92f66973ffb06b532b8b713448 [formerly 898444608438bc92f66973ffb06b532b8b713448 [formerly 898444608438bc92f66973ffb06b532b8b713448 [formerly 4e7a7ea29c1b8dda52331382577d9ffd91ba2466]]]]]]]
    Former-commit-id: da162642996c3802874bdf99bb1b519e3f55b9db [formerly 184de12e23e8dfc1653e59c453c0360f4a09485f] [formerly ad16a7e7eda5518c5a5ae6a03a7ad4827124c50a [formerly ad16a7e7eda5518c5a5ae6a03a7ad4827124c50a [formerly ad16a7e7eda5518c5a5ae6a03a7ad4827124c50a [formerly ad16a7e7eda5518c5a5ae6a03a7ad4827124c50a [formerly ad16a7e7eda5518c5a5ae6a03a7ad4827124c50a [formerly d08279632eea24e117e3f69b6bb24ac8c9872249]]]]]]
    Former-commit-id: ad16a7e7eda5518c5a5ae6a03a7ad4827124c50a
    Former-commit-id: a215989e7e6895bb55948b0b4851c0c195d311f9
    Former-commit-id: 0dbd93419374d63b2813f1bc162ead9ec37f015f [formerly 07273418842e4d9ecb94d943cd8b3a102a4a5773]
    Former-commit-id: 1322d06b836bde0e596d26b2b718ac116deb9b96
    Former-commit-id: 75266cafe90da97d685e8945b7ea0213ad6f449e [formerly f14b33514d23e3109cd5ea840565909b0ac25bcc]
    Former-commit-id: 4e49a5b7396695af9929cbae6a97149c79533bad

--- scripts/vm-initial-setup/basicInstallations.sh
+++ scripts/vm-initial-setup/basicInstallations.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 #This script will install basic software on the vm
 
-#echo "Installing APT"
+#echo '================================================================================'
+#echo $'			Installing APT'
+#echo '================================================================================'
 #wget http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm
 #rpm -i rpmforge-release-0.5.*.rpm
 #yum install apt
 #sudo apt-get update
 
-echo ...........................Installing telnet...........................
+echo '================================================================================'
+echo $'			Installing telnet'
+echo '================================================================================'
 if [ -f "/vshare/base-images/misc/telnet-0.17-59.el7.x86_64.rpm" ]; then
 	yum install -y /vshare/base-images/misc/telnet-0.17-59.el7.x86_64.rpm
 else
 	yum install -y telnet
 fi
 
-echo ...........................Installing wget...........................
+echo '================================================================================'
+echo $'			Installing wget'
+echo '================================================================================'
 if [ -f "/vshare/base-images/misc/wget-1.14-10.el7.x86_64.rpm" ]; then
 	yum install -y /vshare/base-images/misc/wget-1.14-10.el7.x86_64.rpm
 else
