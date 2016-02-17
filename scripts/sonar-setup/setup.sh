3c555e48d0bfa76ba6d68386464748c6e95932dd
Krishna Kuntala <krishna.kuntala@mastek.com>
2016-02-17 14:01:06 +0000

    Added echo formatter.
    Former-commit-id: 6f10fe31862ce8a5764be63a7174fae24344bd77 [formerly 6f8dae43279279af7d211951d99e4418eb836f51] [formerly 392edb3bfa879a4247ccbfdc308425ea5f874450 [formerly e97cab83f0c66134cee3d803672e659e8eae5b9a]] [formerly 27226c3ab236581fcacf9f996b65839a3962fe0b] [formerly 7c4dfb1bd0bb40973c54971a78071f6b44905993 [formerly 68d2fdf4e3296eb6100e9f4d99a3f1433bfea925]] [formerly 1121e7771f35318e93250f0f592028cd56a0f7bf] [formerly 1955bb7eac2d05c2659a80a405733c44b0d5fc07] [formerly 3551f21885c3eacbf0e5d1d59222df3bdd3526f7 [formerly 5009a91cf7d8955e28816d2740c6c563453b3939] [formerly 1955bb7eac2d05c2659a80a405733c44b0d5fc07 [formerly 1955bb7eac2d05c2659a80a405733c44b0d5fc07 [formerly 1955bb7eac2d05c2659a80a405733c44b0d5fc07 [formerly 1955bb7eac2d05c2659a80a405733c44b0d5fc07 [formerly 1955bb7eac2d05c2659a80a405733c44b0d5fc07 [formerly 1984cffdc14ac7f6f73c5174cc1b59bad9abe6a7]]]]]]]
    Former-commit-id: e75df0e98fab41d9e809a7310f2499823e87d1b0 [formerly 6550175f48bca201f09f99f9fc137c5467936422] [formerly 41c10facadf916af47502104430de487e41c3ddc [formerly 08a46b88d0af76562ea97400283fb0af16649694] [formerly 41c10facadf916af47502104430de487e41c3ddc [formerly 08a46b88d0af76562ea97400283fb0af16649694] [formerly 41c10facadf916af47502104430de487e41c3ddc [formerly 08a46b88d0af76562ea97400283fb0af16649694] [formerly 41c10facadf916af47502104430de487e41c3ddc [formerly 08a46b88d0af76562ea97400283fb0af16649694] [formerly 08a46b88d0af76562ea97400283fb0af16649694 [formerly 8459222a38840698a26b47774982a0c2818fb5a2]]]]]]
    Former-commit-id: 41c10facadf916af47502104430de487e41c3ddc [formerly 41c10facadf916af47502104430de487e41c3ddc [formerly 08a46b88d0af76562ea97400283fb0af16649694]]
    Former-commit-id: 30007a72008de628edba7cd0dc9d2b4775406ce3
    Former-commit-id: cc912063364a74cb96b2e8dd8e5551e3ef60b61c [formerly 6e4aa118bc2da9bed4cd6888b3730b402ea8328d]
    Former-commit-id: 4c8365c96c3b5599ac8ac2eb81507d29dc3c01d1
    Former-commit-id: 1a8aa00616986c058c7926d0105b82b29b117873 [formerly e6610d7dbd933119f01251b22788591d04833ff5]
    Former-commit-id: 1534d467db7dbe0129f1a26d8e556cef74a85e43

--- scripts/sonar-setup/setup.sh
+++ scripts/sonar-setup/setup.sh
@@ -6,19 +6,27 @@ mkdir /opt/sonar
 chmod 777 /opt/sonar
 cd /opt/sonar
 
-echo ...........................Installing Unzip...........................
+echo '================================================================================'
+echo '			Installing Unzip'
+echo '================================================================================'
 if [ -f "/vshare/base-images/misc/unzip-6.0-13.el7.x86_64.rpm" ]; then
 	yum install -y /vshare/base-images/misc/unzip-6.0-13.el7.x86_64.rpm
 else
 	yum install -y unzip
 fi
 
-echo ...........................Installing Sonarqube...........................
+echo '================================================================================'
+echo '			Installing Sonarqube: START'
+echo '================================================================================'
 if [ -f "/vshare/base-images/sonarqube/sonarqube-5.1.zip" ]; then
 	cp /vshare/base-images/sonarqube/sonarqube-5.1.zip .
 else
 	wget http://dist.sonar.codehaus.org/sonarqube-5.1.zip
 fi
+
+echo '================================================================================'
+echo '			Installing Sonarqube: END'
+echo '================================================================================'
 
 chmod 777 sonarqube-5.1.zip
 unzip sonarqube-5.1.zip
