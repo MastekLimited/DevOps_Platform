eba281002c9a2a9425ba6609b91882fdbea5a286
Krishna Kuntala <krishna.kuntala@mastek.com>
2016-02-17 14:28:20 +0000

    Modified echo statements.
    Former-commit-id: c2f834617392f2758a3a5bc6ebffb58a199ec095

--- scripts/jenkins-setup/job-config/artifacts-transfer.sh
+++ scripts/jenkins-setup/job-config/artifacts-transfer.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo '================================================================================'
+echo '			Transferring build artifacts'
+echo '================================================================================'
 echo ">>>>>>>>>>>>>>>> Transferring Artifacts >>>>>>>>>>>>>>>>>>>>"
 scp  -o StrictHostKeyChecking=no /var/lib/jenkins/jobs/DevOps_Development/workspace/Microservices/uuid-service/target/uuidService.jar vagrant@&&DOCKER_HOST_IP&&:/mnt/gluster/repo/Build/uuid
 scp  -o StrictHostKeyChecking=no /var/lib/jenkins/jobs/DevOps_Development/workspace/Microservices/employee-service/target/employeeService.jar vagrant@&&DOCKER_HOST_IP&&:/mnt/gluster/repo/Build/employee
@@ -7,8 +10,12 @@ scp  -o StrictHostKeyChecking=no /var/lib/jenkins/jobs/DevOps_Development/worksp
 scp  -o StrictHostKeyChecking=no /var/lib/jenkins/jobs/DevOps_Development/workspace/Microservices/device-registration-service/target/deviceRegistrationService.jar vagrant@&&DOCKER_HOST_IP&&:/mnt/gluster/repo/Build/device-registration
 scp  -o StrictHostKeyChecking=no /var/lib/jenkins/jobs/DevOps_Development/workspace/Microservices/device-authentication-service/target/deviceAuthenticationService.jar vagrant@&&DOCKER_HOST_IP&&:/mnt/gluster/repo/Build/device-authentication
 scp  -o StrictHostKeyChecking=no /var/lib/jenkins/jobs/DevOps_Development/workspace/WebApplications/organisation-web-application/target/organisationWebApp.war vagrant@&&DOCKER_HOST_IP&&:/mnt/gluster/repo/Build/organisation-web
-echo ">>>>>>>>>>>>>>>> Transferred Artifacts >>>>>>>>>>>>>>>>>>>>"
-echo ".................. Restart the docker containers:START ........................"
+
+echo '================================================================================'
+echo '			Restart the docker containers: START'
+echo '================================================================================'
 ssh -o StrictHostKeyChecking=no vagrant@&&DOCKER_HOST_IP&& 'cd /vshare/docker/ && ./runAllScripts.sh'
-echo ".................. Restart the docker containers:END ........................"
+echo '================================================================================'
+echo '			Restart the docker containers: END'
+echo '================================================================================'
 exit
