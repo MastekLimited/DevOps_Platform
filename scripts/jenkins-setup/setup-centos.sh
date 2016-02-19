85abab5d969033352a9c3977291f5ab246f2e109
Krishna Kuntala <krishna.kuntala@mastek.com>
2016-02-19 15:53:04 +0000

    Moved java directory.
    Former-commit-id: ffca32e326e862248a61b3edef9ce549332a2210 [formerly 2c2b6f88ec8dcfde50a0f96189ddac908efbd0d7] [formerly 05f265a6428c07f0bb648c2e2d7dbba6ed400c0e [formerly 05f265a6428c07f0bb648c2e2d7dbba6ed400c0e [formerly 05f265a6428c07f0bb648c2e2d7dbba6ed400c0e [formerly 05f265a6428c07f0bb648c2e2d7dbba6ed400c0e [formerly 7994a37c35e9d174ad44fe70b199845f1c68be2a]]]]]
    Former-commit-id: 05f265a6428c07f0bb648c2e2d7dbba6ed400c0e
    Former-commit-id: 8babb5ba878cf87cb1405140492ddd66fa80e0c8
    Former-commit-id: b0d6b68b5c6f86784571256cf6de6d8b4604459f [formerly 4870192bcb572f070b0a0381a83a114d734123cb]
    Former-commit-id: 22547bedc03abb0152b425a0f7f3ec6cb77ed9b0
    Former-commit-id: b19573e6ef6a038aaa9bbf2e0a8a1953fef1d089

--- scripts/jenkins-setup/setup-centos.sh
+++ scripts/jenkins-setup/setup-centos.sh
@@ -5,8 +5,8 @@ cd /opt;
 echo '================================================================================'
 echo '			Installing java'
 echo '================================================================================'
-if [ -f "/vshare/repo/jdk/jdk-8u45-linux-x64.rpm" ]; then
-	yum install -y /vshare/repo/jdk/jdk-8u45-linux-x64.rpm
+if [ -f "/vshare/repo/java/jdk-8u45-linux-x64.rpm" ]; then
+	yum install -y /vshare/repo/java/jdk-8u45-linux-x64.rpm
 else
 	wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u45-b14/jdk-8u45-linux-x64.rpm";
 	rpm -Uvh jdk-8u45-linux-x64.rpm;
