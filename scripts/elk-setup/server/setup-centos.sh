dd49d8f4906e688ce3e4c16b6af8b9ebc5e93d41
Krishna Kuntala <krishna.kuntala@mastek.com>
2016-02-19 15:53:04 +0000

    Moved java directory.
    Former-commit-id: ffca32e326e862248a61b3edef9ce549332a2210 [formerly 2c2b6f88ec8dcfde50a0f96189ddac908efbd0d7] [formerly 598c5ebcdf878353f1b271be43fed20894d9c7a9 [formerly 05f265a6428c07f0bb648c2e2d7dbba6ed400c0e] [formerly 598c5ebcdf878353f1b271be43fed20894d9c7a9 [formerly 05f265a6428c07f0bb648c2e2d7dbba6ed400c0e] [formerly 598c5ebcdf878353f1b271be43fed20894d9c7a9 [formerly 05f265a6428c07f0bb648c2e2d7dbba6ed400c0e] [formerly 598c5ebcdf878353f1b271be43fed20894d9c7a9 [formerly 05f265a6428c07f0bb648c2e2d7dbba6ed400c0e] [formerly 598c5ebcdf878353f1b271be43fed20894d9c7a9 [formerly 05f265a6428c07f0bb648c2e2d7dbba6ed400c0e] [formerly 05f265a6428c07f0bb648c2e2d7dbba6ed400c0e [formerly 7994a37c35e9d174ad44fe70b199845f1c68be2a]]]]]]]
    Former-commit-id: 598c5ebcdf878353f1b271be43fed20894d9c7a9 [formerly 05f265a6428c07f0bb648c2e2d7dbba6ed400c0e]
    Former-commit-id: 8babb5ba878cf87cb1405140492ddd66fa80e0c8
    Former-commit-id: b0d6b68b5c6f86784571256cf6de6d8b4604459f [formerly 4870192bcb572f070b0a0381a83a114d734123cb]
    Former-commit-id: 22547bedc03abb0152b425a0f7f3ec6cb77ed9b0
    Former-commit-id: c50e610e3caaf4a44ea70451ee6521912b972371 [formerly 4df10942cc10b73faf67571e4e271cd2ea54137c] [formerly 85abab5d969033352a9c3977291f5ab246f2e109 [formerly b19573e6ef6a038aaa9bbf2e0a8a1953fef1d089]]
    Former-commit-id: 3ac9a1467076227eb2ec492eba024917de3d90a0
    Former-commit-id: c74c9c580e35d58800ecde71dee0fd37d0193e0e

--- scripts/elk-setup/server/setup-centos.sh
+++ scripts/elk-setup/server/setup-centos.sh
@@ -3,8 +3,8 @@ TLS_DIR=/etc/pki/tls
 echo '================================================================================'
 echo '			Installing java'
 echo '================================================================================'
-if [ -f "/vshare/repo/jdk/jdk-8u45-linux-x64.rpm" ]; then
-	sudo yum install -y /vshare/repo/jdk/jdk-8u45-linux-x64.rpm
+if [ -f "/vshare/repo/java/jdk-8u45-linux-x64.rpm" ]; then
+	sudo yum install -y /vshare/repo/java/jdk-8u45-linux-x64.rpm
 else
 	sudo wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u45-b14/jdk-8u45-linux-x64.rpm";
 	sudo rpm -Uvh jdk-8u45-linux-x64.rpm;
