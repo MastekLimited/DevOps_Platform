183aeb2385aa93e29bae0a8eee367c59ac20c379
govindp <govind.parashar@mastek.com>
2016-06-10 16:24:15 +0100

    Added SVN Script

--- scripts/svn-setup/svn-setup.sh
+++ scripts/svn-setup/svn-setup.sh
@@ -7,10 +7,10 @@ touch /etc/httpd/conf.d/subversion.conf
 chmod 777 /etc/httpd/conf.d/subversion.conf
 cp /mnt/gluster/repo/subversion.conf /etc/httpd/conf.d/subversion.conf
 mkdir -p /opt/svn
-chmod -R 777 /opt/svn
+cd /opt/svn
 svnadmin create testrepo1
 svnadmin create testrepo2
-chown -R /opt/svn/*
+chown -R apache.apache /opt/svn/*
 touch /etc/svn-users
 htpasswd -b /etc/svn-users user1 Password1
 service httpd restart
