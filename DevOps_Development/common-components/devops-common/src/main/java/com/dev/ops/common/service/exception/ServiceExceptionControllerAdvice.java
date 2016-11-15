4d3d64c5e3191594436d03d52981c2d51c87f09b
Krishna Kuntala <krishna.kuntala@mastek.com>
2016-11-15 15:02:28 +0000

    Changed validation errors handling mechanism.

--- DevOps_Development/common-components/devops-common/src/main/java/com/dev/ops/common/service/exception/ServiceExceptionControllerAdvice.java
+++ DevOps_Development/common-components/devops-common/src/main/java/com/dev/ops/common/service/exception/ServiceExceptionControllerAdvice.java
@@ -35,7 +35,7 @@ public class ServiceExceptionControllerAdvice extends ResponseEntityExceptionHan
 			validationErrors.append("Field error in object '" + fieldError.getObjectName() + "' on field '" + fieldError.getField() + "': rejected value [" + fieldError.getRejectedValue() + "] with cause: [" + fieldError.getDefaultMessage() + " ]");
 		}
 		final Exception e = ExceptionUtil.createException(validationErrors.toString(), Severity.FATAL, ex, null, true, null);
-		return this.handleExceptionInternal(e, validationErrors.toString(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+		return this.handleExceptionInternal(e, validationErrors.toString(), headers, status, request);
 	}
 
 	@ExceptionHandler({PersistenceException.class, CannotCreateTransactionException.class, TransactionSystemException.class})
